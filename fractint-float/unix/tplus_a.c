9fd96ff20abcd01a0ea91ee3659454682ba691df
None <None@b191ffbd-432e-0410-afe6-bde6f7999b1f>
2006-03-11 19:23:10 +0000

    This commit was manufactured by cvs2svn to create tag 'fractint-20-04p04'.

--- fractint-float/unix/tplus_a.c
+++
@@ -1,9 +0,0 @@
-/* tplus_a.c
- * This file contains routines to replace tplus_a.asm.
- *
- * This file Copyright 1992 Ken Shirriff.  It may be used according to the
- * fractint license conditions, blah blah blah.
- */
-
-int WriteTPlusBankedPixel() {return(0);}
-int ReadTPlusBankedPixel() {return(0);}
