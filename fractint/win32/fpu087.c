4774f0cabd602726258b76295e0f3d59e476d4b7
legalize <legalize@b191ffbd-432e-0410-afe6-bde6f7999b1f>
2007-01-14 21:39:11 +0000

    Normalize Win32 code.

--- fractint/win32/fpu087.c
+++ fractint/win32/fpu087.c
@@ -27,7 +27,7 @@ double PointFive = 0.5;
 
 void FPUaptan387(double *y, double *x, double *atan)
 {
-    *atan = atan2(*y,*x);
+    *atan = atan2(*y, *x);
 }
 
 void FPUcplxmul(_CMPLX *x, _CMPLX *y, _CMPLX *z)
@@ -40,13 +40,14 @@ void FPUcplxmul(_CMPLX *x, _CMPLX *y, _CMPLX *z)
 
 void FPUcplxdiv(_CMPLX *x, _CMPLX *y, _CMPLX *z)
 {
-    double mod,tx,yxmod,yymod;
+    double mod, tx, yxmod, yymod;
     mod = y->x * y->x + y->y * y->y;
-    if (mod==0) {
-	DivideOverflow++;
+    if (mod == 0)
+	{
+		DivideOverflow++;
     }
-    yxmod = y->x/mod;
-    yymod = - y->y/mod;
+    yxmod = y->x / mod;
+    yymod = - y->y / mod;
     tx = x->x * yxmod - x->y * yymod;
     z->y = x->x * yymod + x->y * yxmod;
     z->x = tx;
@@ -66,10 +67,10 @@ void FPUsinhcosh(double *Angle, double *Sinh, double *Cosh)
 
 void FPUcplxlog(_CMPLX *x, _CMPLX *z)
 {
-    double mod,zx,zy;
+    double mod, zx, zy;
     mod = sqrt(x->x*x->x + x->y*x->y);
     zx = log(mod);
-    zy = atan2(x->y,x->x);
+    zy = atan2(x->y, x->x);
 
     z->x = zx;
     z->y = zy;
@@ -77,7 +78,7 @@ void FPUcplxlog(_CMPLX *x, _CMPLX *z)
 
 void FPUcplxexp387(_CMPLX *x, _CMPLX *z)
 {
-    double pow,y;
+    double pow, y;
     y = x->y;
     pow = exp(x->x);
     z->x = pow*cos(y);
@@ -88,100 +89,86 @@ void FPUcplxexp387(_CMPLX *x, _CMPLX *z)
 void SinCos086(long x, long *sinx, long *cosx)
 {
     double a;
-    a = x/(double)(1<<16);
-    *sinx = (long) (sin(a)*(double)(1<<16));
-    *cosx = (long) (cos(a)*(double)(1<<16));
+    a = x/(double)(1 << 16);
+    *sinx = (long) (sin(a)*(double) (1 << 16));
+    *cosx = (long) (cos(a)*(double) (1 << 16));
 }
 
 void SinhCosh086(long x, long *sinx, long *cosx)
 {
     double a;
-    a = x/(double)(1<<16);
-    *sinx = (long) (sinh(a)*(double)(1<<16));
-    *cosx = (long) (cosh(a)*(double)(1<<16));
+    a = x / (double) (1 << 16);
+    *sinx = (long) (sinh(a)*(double) (1 << 16));
+    *cosx = (long) (cosh(a)*(double) (1 << 16));
 }
 
-long Exp086(x)
-long x;
+long Exp086(long x)
 {
-    return (long) (exp((double)x/(double)(1<<16))*(double)(1<<16));
+    return (long) (exp((double) x / (double) (1 << 16))*(double) (1 << 16));
 }
 
-#define em2float(l) (*(float *)&(l))
-#define float2em(f) (*(long *)&(f))
+#define em2float(l) (*(float *) &(l))
+#define float2em(f) (*(long *) &(f))
 
 /*
  * Input is a 16 bit offset number.  Output is shifted by Fudge.
  */
-unsigned long ExpFudged(x, Fudge)
-long x;
-int Fudge;
+unsigned long ExpFudged(long x, int Fudge)
 {
-    return (long) (exp((double)x/(double)(1<<16))*(double)(1<<Fudge));
+    return (long) (exp((double) x / (double) (1 << 16))*(double) (1 << Fudge));
 }
 
 /* This multiplies two e/m numbers and returns an e/m number. */
-long r16Mul(x,y)
-long x,y;
+long r16Mul(long x, long y)
 {
-    float f;
-    f = em2float(x)*em2float(y);
+	float f = em2float(x)*em2float(y);
     return float2em(f);
 }
 
 /* This takes an exp/mant number and returns a shift-16 number */
-long LogFloat14(x)
-unsigned long x;
+long LogFloat14(unsigned long x)
 {
-    return (long) log((double)em2float(x))*(1<<16);
+    return (long) log((double) em2float(x))*(1 << 16);
 }
 
 /* This divides two e/m numbers and returns an e/m number. */
-long RegDivFloat(x,y)
-long x,y;
+long RegDivFloat(long x, long y)
 {
-    float f;
-    f = em2float(x)/em2float(y);
+	float f = em2float(x)/em2float(y);
     return float2em(f);
 }
 
 /*
- * This routine on the IBM converts shifted integer x,FudgeFact to 
- * the 4 byte number: exp,mant,mant,mant
+ * This routine on the IBM converts shifted integer x, FudgeFact to 
+ * the 4 byte number: exp, mant, mant, mant
  * Instead of using exp/mant format, we'll just use floats.
  * Note: If sizeof(float) != sizeof(long), we're hosed.
  */
-long RegFg2Float(x,FudgeFact)
-long x;
-int FudgeFact;
+long RegFg2Float(long x, int FudgeFact)
 {
-    float f;
-    long l;
-    f = x/(float)(1<<FudgeFact);
-    l = float2em(f);
-    return l;
+	float f = (float) x / (float) (1 << FudgeFact);
+	return float2em(f);
 }
 
 /*
  * This converts em to shifted integer format.
  */
-long RegFloat2Fg(x,Fudge)
-long x;
-int Fudge;
+long RegFloat2Fg(long x, int Fudge)
 {
-    return (long) (em2float(x)*(float)(1<<Fudge));
+    return (long) (em2float(x)*(float) (1 << Fudge));
 }
 
-long RegSftFloat(x, Shift)
-long x;
-int Shift;
+long RegSftFloat(long x, int Shift)
 {
     float f;
     f = em2float(x);
-    if (Shift>0) {
-	f *= (1<<Shift);
-    } else {
-	f /= (1<<Shift);
+    if (Shift > 0)
+	{
+		f *= (1 << Shift);
+    }
+	else
+	{
+		f /= (1 << Shift);
     }
     return float2em(f);
 }
