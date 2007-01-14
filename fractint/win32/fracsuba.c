d3990c46548a707a0b49415001a261e7134b9676
legalize <legalize@xmission.com>
2007-01-14 21:39:11 +0000

    Normalize Win32 code.

--- fractint/win32/fracsuba.c
+++ fractint/win32/fracsuba.c
@@ -3,25 +3,60 @@
 
 int FManOWarfpFractal(void)
 {
+	static int been_here = 0;
+	if (!been_here)
+	{
+		stopmsg(0, "This integer fractal type is unimplemented;\n"
+			"Use float=yes to get a real image.");
+		been_here = 1;
+	}
 	return 0;
 }
 
 int FJuliafpFractal(void)
 {
+	static int been_here = 0;
+	if (!been_here)
+	{
+		stopmsg(0, "This integer fractal type is unimplemented;\n"
+			"Use float=yes to get a real image.");
+		been_here = 1;
+	}
 	return 0;
 }
 
 int FBarnsley1FPFractal(void)
 {
+	static int been_here = 0;
+	if (!been_here)
+	{
+		stopmsg(0, "This integer fractal type is unimplemented;\n"
+			"Use float=yes to get a real image.");
+		been_here = 1;
+	}
 	return 0;
 }
 
 int FBarnsley2FPFractal(void)
 {
+	static int been_here = 0;
+	if (!been_here)
+	{
+		stopmsg(0, "This integer fractal type is unimplemented;\n"
+			"Use float=yes to get a real image.");
+		been_here = 1;
+	}
 	return 0;
 }
 
 int FLambdaFPFractal(void)
 {
+	static int been_here = 0;
+	if (!been_here)
+	{
+		stopmsg(0, "This integer fractal type is unimplemented;\n"
+			"Use float=yes to get a real image.");
+		been_here = 1;
+	}
 	return 0;
 }
