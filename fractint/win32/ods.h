2f201e711dc99c6bf28ba4c5f1045da9dd38b792
legalize <legalize@b191ffbd-432e-0410-afe6-bde6f7999b1f>
2007-01-15 06:22:10 +0000

    Add trace macros.

---
+++ fractint/win32/ods.h
@@ -0,0 +1,22 @@
+#if !defined(ODS_H)
+#define ODS_H
+
+#define RT_VERBOSE
+#if defined(RT_VERBOSE)
+#define ODS(text_)						ods(__FILE__, __LINE__, text_)
+#define ODS1(fmt_, arg_)				ods(__FILE__, __LINE__, fmt_, arg_)
+#define ODS2(fmt_, a1_, a2_)			ods(__FILE__, __LINE__, fmt_, a1_, a2_)
+#define ODS3(fmt_, a1_, a2_, a3_)		ods(__FILE__, __LINE__, fmt_, a1_, a2_, a3_)
+#define ODS4(fmt_, _1, _2, _3, _4)		ods(__FILE__, __LINE__, fmt_, _1, _2, _3, _4)
+#define ODS5(fmt_, _1, _2, _3, _4, _5)	ods(__FILE__, __LINE__, fmt_, _1, _2, _3, _4, _5)
+extern void ods(const char *file, unsigned int line, const char *format, ...);
+#else
+#define ODS(text_)
+#define ODS1(fmt_, arg_)
+#define ODS2(fmt_, a1_, a2_)
+#define ODS3(fmt_, a1_, a2_, a3_)
+#define ODS4(fmt_, _1, _2, _3, _4)
+#define ODS5(fmt_, _1, _2, _3, _4, _5)
+#endif
+
+#endif
