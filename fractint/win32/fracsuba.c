6af6cfb9f503e816e6954c311898d42946b283eb
legalize <legalize@xmission.com>
2007-01-28 00:55:09 +0000

    Call floating-point C implementations when someone tries to call the assembly language implementations.

--- fractint/win32/fracsuba.c
+++ fractint/win32/fracsuba.c
@@ -3,60 +3,25 @@
 
 int FManOWarfpFractal(void)
 {
-	static int been_here = 0;
-	if (!been_here)
-	{
-		stopmsg(0, "This integer fractal type is unimplemented;\n"
-			"Use float=yes to get a real image.");
-		been_here = 1;
-	}
-	return 0;
+	return ManOWarfpFractal();
 }
 
 int FJuliafpFractal(void)
 {
-	static int been_here = 0;
-	if (!been_here)
-	{
-		stopmsg(0, "This integer fractal type is unimplemented;\n"
-			"Use float=yes to get a real image.");
-		been_here = 1;
-	}
-	return 0;
+	return JuliafpFractal();
 }
 
 int FBarnsley1FPFractal(void)
 {
-	static int been_here = 0;
-	if (!been_here)
-	{
-		stopmsg(0, "This integer fractal type is unimplemented;\n"
-			"Use float=yes to get a real image.");
-		been_here = 1;
-	}
-	return 0;
+	return Barnsley1FPFractal();
 }
 
 int FBarnsley2FPFractal(void)
 {
-	static int been_here = 0;
-	if (!been_here)
-	{
-		stopmsg(0, "This integer fractal type is unimplemented;\n"
-			"Use float=yes to get a real image.");
-		been_here = 1;
-	}
-	return 0;
+	return Barnsley2FPFractal();
 }
 
 int FLambdaFPFractal(void)
 {
-	static int been_here = 0;
-	if (!been_here)
-	{
-		stopmsg(0, "This integer fractal type is unimplemented;\n"
-			"Use float=yes to get a real image.");
-		been_here = 1;
-	}
-	return 0;
+	return LambdaFPFractal();
 }
