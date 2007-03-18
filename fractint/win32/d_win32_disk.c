2adac1495b3db4b873068c07e5f26c5f06f1a467
legalize <legalize@xmission.com>
2007-03-18 02:40:53 +0000

    Add driver_{get,put}_char_attr_rowcol

--- fractint/win32/d_win32_disk.c
+++ fractint/win32/d_win32_disk.c
@@ -1071,6 +1071,7 @@ static Win32DiskDriver disk_driver_info =
 		win32_init_fm, win32_buzzer, win32_sound_on, win32_sound_off, win32_mute,
 		disk_diskp,
 		win32_get_char_attr, win32_put_char_attr,
+		win32_get_char_attr_rowcol, win32_put_char_attr_rowcol,
 		win32_delay,
 		win32_set_keyboard_timeout,
 		disk_flush
