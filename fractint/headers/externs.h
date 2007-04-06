6aa84cdd9efefa6818f107b4add8f74f04cddad3
legalize <legalize@xmission.com>
2007-04-06 13:34:08 +0000

    Normalize procedures and data from calcfrac.c: g_attractors_l

--- fractint/headers/externs.h
+++ fractint/headers/externs.h
@@ -236,7 +236,7 @@ extern int                   keybuffer;
 extern int                   LastInitOp;
 extern unsigned              LastOp;
 extern int                   lastorbittype;
-extern _LCMPLX               lattr[];
+extern _LCMPLX               g_attractors_l[];
 extern long                  g_close_enough_l;
 extern _LCMPLX               lcoefficient;
 extern int                   ldcheck;
