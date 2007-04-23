db93b7c2cec3aae785f046d6ba0b7a7c798d6d46
legalize <legalize@b191ffbd-432e-0410-afe6-bde6f7999b1f>
2007-04-23 07:39:10 +0000

    Remove unused fracsuba.cpp

--- fractint/unix/fracsuba.cpp
+++
@@ -1,8 +0,0 @@
-#include "port.h"
-#include "prototyp.h"
-
-int FManOWarfpFractal(void) {return(0);}
-int FJuliafpFractal(void) {return(0);}
-int FBarnsley1FPFractal(void) {return(0);}
-int FBarnsley2FPFractal(void) {return(0);}
-int FLambdaFPFractal(void) {return(0);}
