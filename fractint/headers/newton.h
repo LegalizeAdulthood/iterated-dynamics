d4c610dedc2da9be23efd2835ed5ad3dfea31f97
legalize <legalize@xmission.com>
2007-04-27 13:31:50 +0000

    Localize fractal-specific globals.

--- fractint/headers/newton.h
+++ fractint/headers/newton.h
@@ -4,8 +4,21 @@
 class Newton
 {
 public:
+	Newton()
+		: m_root_over_degree(0.0),
+		m_degree_minus_1_over_degree(0.0)
+	{
+	}
 	int setup();
 	virtual int orbit();
+
+protected:
+	struct MP m_degree_minus_1_over_degree_mp;
+	struct MP m_root_over_degree_mp;
+
+private:
+	double m_root_over_degree;
+	double m_degree_minus_1_over_degree;
 };
 
 class NewtonMPC : public Newton
