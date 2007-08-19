42615555b857aaaa34ed7e7b115d93bec5bc7ae1
legalize <legalize@xmission.com>
2007-08-19 03:10:28 +0000

    Add tests

---
+++ fractint/test/idTests/stdafx.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+#include "CppUnitLite/TestHarness.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+extern SimpleString StringFrom(const std::string &text);
