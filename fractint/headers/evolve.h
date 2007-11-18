d634c2f5b87864e38a56aef5656a1ec2bc781ada
legalize <legalize@xmission.com>
2007-11-18 02:55:24 +0000

    Convert int to bool.

--- fractint/headers/evolve.h
+++ fractint/headers/evolve.h
@@ -8,7 +8,7 @@ extern  void set_current_parameters();
 extern  void fiddle_parameters(GENEBASE gene[], int ecount);
 extern  void set_evolve_ranges();
 extern  void set_mutation_level(int);
-extern  void draw_parameter_box(int);
+extern  void draw_parameter_box(bool clear_not_redraw);
 extern  void spiral_map(int);
 extern  int unspiral_map();
 extern  void setup_parameter_box();
