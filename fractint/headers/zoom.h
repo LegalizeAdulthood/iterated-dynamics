fb872a40ae177df8650ba0793459e58e7bd6a189
legalize <legalize@b191ffbd-432e-0410-afe6-bde6f7999b1f>
2007-11-18 13:16:27 +0000

    Replace integer with boolean. Remove unused return value.

--- fractint/headers/zoom.h
+++ fractint/headers/zoom.h
@@ -1,13 +1,13 @@
 #if !defined(ZOOM_H)
 #define ZOOM_H
 
-extern void zoom_box_draw(int);
+extern void zoom_box_draw(bool drawit);
 extern void zoom_box_move(double, double);
 extern void zoom_box_resize(int);
 extern void zoom_box_change_i(int, int);
 extern void zoom_box_out();
 extern void aspect_ratio_crop(float, float);
-extern int init_pan_or_recalc(int);
+extern void init_pan_or_recalc(bool do_zoomout);
 extern void _fastcall draw_lines(Coordinate, Coordinate, int, int);
 extern void _fastcall add_box(Coordinate);
 
