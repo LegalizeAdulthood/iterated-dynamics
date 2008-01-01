e7e5b0bcd04628fe89878d868523bb835b1845a2
legalize <legalize@b191ffbd-432e-0410-afe6-bde6f7999b1f>
2008-01-01 20:00:20 +0000

    Pass structures by const pointer.

--- fractint/headers/loadfdos.h
+++ fractint/headers/loadfdos.h
@@ -1,5 +1,5 @@
 #if !defined(LOAD_F_DOS_H)
 
-extern int get_video_mode(const fractal_info *, struct ext_blk_formula_info *);
+extern int get_video_mode(fractal_info const *info, struct ext_blk_formula_info const *formula_info);
 
 #endif
