4b48fad4f35eb0a677590e2445a70f8fe7f7e0d6
legalize <legalize@b191ffbd-432e-0410-afe6-bde6f7999b1f>
2008-01-02 03:27:43 +0000

    Split disk view parameter screen from view window parameter screen. Move view window parameter screen to ViewWindow.

--- fractint/common/ViewWindow.cpp
+++ fractint/common/ViewWindow.cpp
@@ -7,7 +7,10 @@
 #include "id.h"
 #include "externs.h"
 
+#include "helpdefs.h"
+#include "UIChoices.h"
 #include "ViewWindow.h"
+#include "zoom.h"
 
 using namespace boost;
 
@@ -67,5 +70,70 @@ void ViewWindow::InitializeRestart()
 	_width = 0;
 	_height = 0;
 }
+
+int ViewWindow::GetParameters()
+{
+	bool old_viewwindow = _visible;
+	float old_viewreduction = _reduction;
+	float old_aspectratio = _aspectRatio;
+	int old_viewxdots = _width;
+	int old_viewydots = _height;
+
+get_view_restart:
+	{
+		UIChoices dialog(HELPVIEW, "View Window Options", 16);
+
+		dialog.push("Preview display? (no for full screen)", _visible);
+		dialog.push("Auto window size reduction factor", _reduction);
+		dialog.push("Final media overall aspect ratio, y/x", _aspectRatio);
+		dialog.push("Crop starting coordinates to new aspect ratio?", _viewCrop);
+		dialog.push("Explicit size x pixels (0 for auto size)", _width);
+		dialog.push("              y pixels (0 to base on aspect ratio)", _height);
+		dialog.push("");
+		dialog.push("Press F4 to reset view parameters to defaults.");
+
+		int i = dialog.prompt();
+		if (i < 0)
+		{
+			return -1;
+		}
+
+		if (i == FIK_F4)
+		{
+			InitializeRestart();
+			goto get_view_restart;
+		}
+
+		int k = -1;
+		_visible = (dialog.values(++k).uval.ch.val != 0);
+		_reduction = float(dialog.values(++k).uval.dval);
+		_aspectRatio = float(dialog.values(++k).uval.dval);
+		_viewCrop = (dialog.values(++k).uval.ch.val != 0);
+		_width = dialog.values(++k).uval.ival;
+		_height = dialog.values(++k).uval.ival;
+	}
+
+	if (_width != 0 && _height != 0 && _visible && _aspectRatio == 0.0f)
+	{
+		_aspectRatio = float(_height)/float(_width);
+	}
+	else if (_aspectRatio == 0.0 && (_width == 0 || _height == 0))
+	{
+		_aspectRatio = old_aspectratio;
+	}
+
+	if (_aspectRatio != old_aspectratio && _viewCrop)
+	{
+		aspect_ratio_crop(old_aspectratio, _aspectRatio);
+	}
+
+	return (_visible != old_viewwindow
+		|| (_visible
+			&& (_reduction != old_viewreduction
+				|| _aspectRatio != old_aspectratio
+				|| _width != old_viewxdots
+				|| (_height != old_viewydots && _width)))) ? 1 : 0;
+}
+
 
 ViewWindow g_viewWindow;
