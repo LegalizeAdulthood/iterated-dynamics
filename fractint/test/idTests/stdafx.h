331648918b333848fe26a1c50aadff7a2dffb4f7
legalize <legalize@b191ffbd-432e-0410-afe6-bde6f7999b1f>
2008-03-23 20:00:34 +0000

    Switch to boost unit test framework.

--- fractint/test/idTests/stdafx.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-
-#include "CppUnitLite/TestHarness.h"
-
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-
-extern SimpleString StringFrom(const std::string &text);
