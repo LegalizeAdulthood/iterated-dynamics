acd156ce28580d6b428e3f9bb56f9e8771faefa9
legalize <legalize@b191ffbd-432e-0410-afe6-bde6f7999b1f>
2008-03-23 20:18:40 +0000

    Switch to boost unit test framework.

--- fractint/test/idTests/TestFullScreenChooser.cpp
+++ fractint/test/idTests/TestFullScreenChooser.cpp
@@ -15,14 +15,49 @@
 class FakeFullScreenChooserApp : public IFullScreenChooserApp
 {
 public:
+	FakeFullScreenChooserApp() : _helpTitleCalled(false),
+		_putStringCenterCalled(false),
+		_putStringCenterLastRow(0),
+		_putStringCenterLastColumn(0),
+		_putStringCenterLastWidth(0),
+		_putStringCenterLastAttr(0),
+		_putStringCenterLastMessage(),
+		_putStringCenterFakeResult(0)
+	{ }
 	virtual ~FakeFullScreenChooserApp() { }
 
 	virtual int put_string_center(int row, int col, int width, int attr, const char *msg)
-	{ throw not_implemented("put_string_center"); }
+	{
+		_putStringCenterCalled = true;
+		_putStringCenterLastRow = row;
+		_putStringCenterLastColumn = col;
+		_putStringCenterLastWidth = width;
+		_putStringCenterLastAttr = attr;
+		_putStringCenterLastMessage = msg;
+		return _putStringCenterFakeResult;
+	}
+	void SetPutStringCenterFakeResult(int value)	{ _putStringCenterFakeResult = value; }
+	bool PutStringCenterCalled() const				{ return _putStringCenterCalled; }
+	int PutStringCenterLastRow() const				{ return _putStringCenterLastRow; }
+	int PutStringCenterLastColumn() const			{ return _putStringCenterLastColumn; }
+	int PutStringCenterLastWidth() const			{ return _putStringCenterLastWidth; }
+	int PutStringCenterLastAttr() const				{ return _putStringCenterLastAttr; }
+	const std::string &PutStringCenterLastMessage() const { return _putStringCenterLastMessage; }
 	virtual void help_title()
-	{ throw not_implemented("help_title"); }
+	{ _helpTitleCalled = true; }
+	bool HelpTitleCalled() const { return _helpTitleCalled; }
 	virtual bool ends_with_slash(const char *text)
 	{ throw not_implemented("ends_with_slash"); }
+
+private:
+	bool _helpTitleCalled;
+	bool _putStringCenterCalled;
+	int _putStringCenterLastRow;
+	int _putStringCenterLastColumn;
+	int _putStringCenterLastWidth;
+	int _putStringCenterLastAttr;
+	std::string _putStringCenterLastMessage;
+	int _putStringCenterFakeResult;
 };
 
 BOOST_AUTO_TEST_CASE(FullScreenChooser_NoChoices)
@@ -54,5 +89,6 @@ BOOST_AUTO_TEST_CASE(FullScreenChooser_Escape)
 	fakeDriver.SetKeyStrokes(NUM_OF(keyStrokes), keyStrokes);
 	int result = chooser.Execute();
 	BOOST_CHECK_EQUAL(-1, result);
+	BOOST_CHECK(app.HelpTitleCalled());
 }
 
