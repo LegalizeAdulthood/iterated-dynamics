37e7dadd781ea16d8f3e47034f73d2603a3e9237
legalize <legalize@xmission.com>
2008-09-24 05:22:27 +0000

    Add fitnesse test fixture

---
+++ id/src/test/idFitnesse/VerifyParameterSetFixture.h
@@ -0,0 +1,10 @@
+#if !defined(VERIFY_PARAMETER_SET_FIXTURE_H)
+#define VERIFY_PARAMETER_SET_FIXTURE_H
+
+#include "Fit/ColumnFixture.h"
+
+class VerifyParameterSetFixture : public ColumnFixture
+{
+};
+
+#endif
