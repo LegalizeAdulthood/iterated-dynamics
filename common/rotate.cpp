7483e1ae1e0c964df5bcfd9e0d34baaa20652892
Richard <legalize@xmission.com>
2014-12-13 20:10:35 -0700

    Refactor: Replace (void) with ()

--- common/rotate.cpp
+++ common/rotate.cpp
@@ -12,7 +12,7 @@
 
 /* routines in this module      */
 
-static void pauserotate(void);
+static void pauserotate();
 static void set_palette(BYTE start[3], BYTE finish[3]);
 static void set_palette2(BYTE start[3], BYTE finish[3]);
 static void set_palette3(BYTE start[3], BYTE middle[3], BYTE finish[3]);
@@ -441,7 +441,7 @@ void save_palette()
 }
 
 
-bool load_palette(void)
+bool load_palette()
 {
     int oldhelpmode;
     char filename[FILE_MAX_PATH];
