8122a80db91874dc869a15b9fbd216852624cf61
Richard <legalize@xmission.com>
2014-12-14 16:39:01 -0700

    Refactor: Split chained assignment into multiple assignments

--- common/rotate.cpp
+++ common/rotate.cpp
@@ -439,7 +439,9 @@ static void set_palette2(BYTE start[3], BYTE finish[3])
 
 static void set_palette3(BYTE start[3], BYTE middle[3], BYTE finish[3])
 {
-    g_dac_box[0][0] = g_dac_box[0][1] = g_dac_box[0][2] = 0;
+    g_dac_box[0][2] = 0;
+    g_dac_box[0][1] = g_dac_box[0][2];
+    g_dac_box[0][0] = g_dac_box[0][1];
     for (int i = 1; i <= 85; i++)
         for (int j = 0; j < 3; j++) {
             g_dac_box[i][j]     = (BYTE)((i*middle[j] + (86-i)*start[j])/85);
