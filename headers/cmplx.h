76a2dcf212e603f05169cfeaa160ad37933a1b9c
Richard <legalize@xmission.com>
2014-12-21 19:21:42 -0700

    Refactor: Spaces after commas cmplx.h

--- headers/cmplx.h
+++ headers/cmplx.h
@@ -1,21 +1,26 @@
 // various complex number defs
 #ifndef CMPLX_H_DEFINED
 #define CMPLX_H_DEFINED
-struct DHyperComplex {
-    double x,y;
-    double z,t;
+struct DHyperComplex
+{
+    double x, y;
+    double z, t;
 };
-struct LHyperComplex {
-    long x,y;
-    long z,t;
+struct LHyperComplex
+{
+    long x, y;
+    long z, t;
 };
-struct DComplex {
-    double x,y;
+struct DComplex
+{
+    double x, y;
 };
-struct LDComplex {
-    LDBL x,y;
+struct LDComplex
+{
+    LDBL x, y;
 };
-struct LComplex {
-    long x,y;
+struct LComplex
+{
+    long x, y;
 };
 #endif
