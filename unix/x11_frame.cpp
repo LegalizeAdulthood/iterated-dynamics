5db6b824cad1a67aca4125764c589b04c014d48f
Richard <legalize@xmission.com>
2014-12-22 12:43:19 -0700

    Implement x11_frame_window::get_key_press based on win32 frame_get_key_press

--- unix/x11_frame.cpp
+++ unix/x11_frame.cpp
@@ -1,8 +1,29 @@
+#include <cassert>
+
 #include "x11_frame.h"
 
-int x11_frame_window::get_key_press(int option)
+int x11_frame_window::get_key_press(int wait_for_key)
 {
-    return 0;
+    pump_messages(wait_for_key != 0);
+    if (wait_for_key && timed_out_)
+    {
+        return 0;
+    }
+
+    if (keypress_count_ == 0)
+    {
+        assert(wait_for_key == 0);
+        return 0;
+    }
+
+    int i = keypress_buffer_[keypress_tail_];
+    if (++keypress_tail_ >= KEYBUFMAX)
+    {
+        keypress_tail_ = 0;
+    }
+    --keypress_count_;
+
+    return i;
 }
 
 int x11_frame_window::pump_messages(bool wait_flag)
--- unix/x11_frame.h
+++ unix/x11_frame.h
@@ -3,14 +3,36 @@
 
 #include <X11/X.h>
 
+enum
+{
+    KEYBUFMAX = 80
+};
+
+// cppcheck-suppress noConstructor
 class x11_frame_window
 {
 public:
+    x11_frame_window()
+        : timed_out_{},
+        keypress_count_{},
+        keypress_head_{},
+        keypress_tail_{},
+        keypress_buffer_{}
+    {
+    }
+
     int width() const { return 0; }
     int height() const { return 0; }
     Window window() const { return 0; }
     int get_key_press(int option);
     int pump_messages(bool wait_flag);
+
+private:
+    bool timed_out_;
+    int keypress_count_;
+    int keypress_head_;
+    int keypress_tail_;
+    int keypress_buffer_[KEYBUFMAX];
 };
 
 #endif
