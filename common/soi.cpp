7a7330de6c59a92dcdc911bc1d2475f09bd9fb7b
Richard <legalize@xmission.com>
2014-12-24 11:08:47 -0700

    Refactor: Inline macro

--- common/soi.cpp
+++ common/soi.cpp
@@ -363,7 +363,7 @@ interpolate(cre1, midr, cre2, \
 #define GET_SCAN_REAL(x, y) \
 interpolate(cim1, midi, cim2, \
         EVALUATE(cre1, midr, state.br10, state.br11, state.br12, x), \
-        EVALUATE(cre1, midr, state.br20, state.br21, br22, x), \
+        EVALUATE(cre1, midr, state.br20, state.br21, state.br22, x), \
         EVALUATE(cre1, midr, br30, br31, br32, x), y)
 #define GET_SCAN_IMAG(x, y) \
 interpolate(cre1, midr, cre2, \
@@ -531,7 +531,6 @@ static int rhombus(LDBL cre1, LDBL cre2, LDBL cim1, LDBL cim2,
     static long savecolor, color, helpcolor;
     static int x, y, z, savex;
 
-#define br22      state.br22
 #define br30      state.br30
 #define br31      state.br31
 #define br32      state.br32
@@ -669,7 +668,7 @@ static int rhombus(LDBL cre1, LDBL cre2, LDBL cim1, LDBL cim2,
         // finish up the image by scanning the rectangle
 scan:
         INTERPOLATE(cre1, midr, cre2, zre1, zre5, zre2, state.br10, state.br11, state.br12);
-        INTERPOLATE(cre1, midr, cre2, zre6, zre9, zre7, state.br20, state.br21, br22);
+        INTERPOLATE(cre1, midr, cre2, zre6, zre9, zre7, state.br20, state.br21, state.br22);
         INTERPOLATE(cre1, midr, cre2, zre3, zre8, zre4, br30, br31, br32);
 
         INTERPOLATE(cim1, midi, cim2, zim1, zim6, zim3, bi10, bi11, bi12);
