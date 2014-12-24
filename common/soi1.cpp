4bda6049d626f62079569177fef878691fb6f07e
Richard <legalize@xmission.com>
2014-12-24 13:25:30 -0700

    Refactor: Inline macro

--- common/soi1.cpp
+++ common/soi1.cpp
@@ -288,7 +288,6 @@ static int rhombus(double cre1, double cre2, double cim1, double cim2,
     static long savecolor, color, helpcolor;
     static int x, y, z, savex;
 
-#define tzr1      state.tzr1
 #define tzi1      state.tzi1
 #define tzr2      state.tzr2
 #define tzi2      state.tzi2
@@ -506,7 +505,7 @@ scan:
     state.ci1 = 0.75*cim1 + 0.25*cim2;
     state.ci2 = 0.25*cim1 + 0.75*cim2;
 
-    tzr1 = GET_REAL(state.cr1, state.ci1);
+    state.tzr1 = GET_REAL(state.cr1, state.ci1);
     tzi1 = GET_IMAG(state.cr1, state.ci1);
 
     tzr2 = GET_REAL(state.cr2, state.ci1);
@@ -518,7 +517,7 @@ scan:
     tzr4 = GET_REAL(state.cr2, state.ci2);
     tzi4 = GET_IMAG(state.cr2, state.ci2);
 
-    trq1 = tzr1*tzr1;
+    trq1 = state.tzr1*state.tzr1;
     tiq1 = tzi1*tzi1;
 
     trq2 = tzr2*tzr2;
@@ -639,7 +638,7 @@ scan:
               iq9=zim9*zim9;
         */
         // iterate test point
-        SOI_ORBIT(tzr1, trq1, tzi1, tiq1, state.cr1, state.ci1, state.tesc1);
+        SOI_ORBIT(state.tzr1, trq1, tzi1, tiq1, state.cr1, state.ci1, state.tesc1);
         /*
               tzi1=(tzi1+tzi1)*tzr1+ci1;
               tzr1=trq1-tiq1+cr1;
@@ -704,9 +703,9 @@ scan:
         /* now for all test points, check whether they exceed the
         allowed tolerance. if so, subdivide */
         state.l1 = GET_REAL(state.cr1, state.ci1);
-        state.l1 = (tzr1 == 0.0)?
+        state.l1 = (state.tzr1 == 0.0)?
            (state.l1 == 0.0)?1.0:1000.0:
-           state.l1/tzr1;
+           state.l1/state.tzr1;
         if (FABS(1.0 - state.l1) > twidth)
             break;
 
