a717c5081abb66153cf48e28bc9e1ff705c44043
Richard <legalize@xmission.com>
2014-12-24 11:13:00 -0700

    Refactor: Inline macro

--- common/soi.cpp
+++ common/soi.cpp
@@ -368,7 +368,7 @@ interpolate(cim1, midi, cim2, \
 #define GET_SCAN_IMAG(x, y) \
 interpolate(cre1, midr, cre2, \
         EVALUATE(cim1, midi, state.bi10, state.bi11, state.bi12, y), \
-        EVALUATE(cim1, midi, bi20, bi21, bi22, y), \
+        EVALUATE(cim1, midi, state.bi20, bi21, bi22, y), \
         EVALUATE(cim1, midi, bi30, bi31, bi32, y), x)
 
 /* compute coefficients of Newton polynomial (b0,..,b2) from
@@ -531,7 +531,6 @@ static int rhombus(LDBL cre1, LDBL cre2, LDBL cim1, LDBL cim2,
     static long savecolor, color, helpcolor;
     static int x, y, z, savex;
 
-#define bi20      state.bi20
 #define bi21      state.bi21
 #define bi22      state.bi22
 #define bi30      state.bi30
@@ -666,7 +665,7 @@ scan:
         INTERPOLATE(cre1, midr, cre2, zre3, zre8, zre4, state.br30, state.br31, state.br32);
 
         INTERPOLATE(cim1, midi, cim2, zim1, zim6, zim3, state.bi10, state.bi11, state.bi12);
-        INTERPOLATE(cim1, midi, cim2, zim5, zim9, zim8, bi20, bi21, bi22);
+        INTERPOLATE(cim1, midi, cim2, zim5, zim9, zim8, state.bi20, bi21, bi22);
         INTERPOLATE(cim1, midi, cim2, zim2, zim7, zim4, bi30, bi31, bi32);
 
         state.restep = (cre2-cre1)/(x2-x1);
