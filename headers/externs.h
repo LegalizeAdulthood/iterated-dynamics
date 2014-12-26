72781bb30b946563597cde1c49badf4ca9c5a97c
Richard <legalize@xmission.com>
2014-12-26 12:45:25 -0700

    Refactor: Remove unused declaration

--- headers/externs.h
+++ headers/externs.h
@@ -464,7 +464,6 @@ extern bool                  Targa_Out;
 extern bool                  Targa_Overlay;
 extern double                tempsqrx;
 extern double                tempsqry;
-extern BYTE                  teststring[];
 extern int                   g_text_cbase;      // g_text_col is relative to this
 extern int                   g_text_col;        // current column in text mode
 extern int                   g_text_rbase;      // g_text_row is relative to this
