52ee38cdbc0c3a3272e47271481cec7c44aa28f4
Richard <legalize@xmission.com>
2018-01-20 23:05:42 -0700

    Refactor: Inline inclusion of biginit.h to big.h

--- headers/big.h
+++ headers/big.h
@@ -241,5 +241,38 @@ extern BFComplex *ComplexPower_bf(BFComplex *t, BFComplex *xx, BFComplex *yy);
 extern BNComplex *ComplexPower_bn(BNComplex *t, BNComplex *xx, BNComplex *yy);
 extern BNComplex *cmplxlog_bn(BNComplex *t, BNComplex *s);
 extern BNComplex *cplxmul_bn(BNComplex *t, BNComplex *x, BNComplex *y);
-#include "biginit.h" // fractint only
+
+#define MATHBITS      32
+#define MATHBYTES     (MATHBITS/8)
+#define NUMVARS       30            // room for this many on stack
+#define CURRENTREZ    1
+#define MAXREZ        0
+// globals
+extern int bnstep, bnlength, intlength, rlength, padding, shiftfactor;
+extern int g_decimals, bflength, rbflength, bfshiftfactor, bfdecimals;
+// used internally by bignum.c routines
+extern bn_t bntmp1, bntmp2, bntmp3, bntmp4, bntmp5, bntmp6;     // rlength
+extern bn_t bntmpcpy1, bntmpcpy2;                               // bnlength
+// used by other routines
+extern bn_t bnxmin, bnxmax, bnymin, bnymax, bnx3rd, bny3rd;     // bnlength
+extern bn_t bnxdel, bnydel, bnxdel2, bnydel2, bnclosenuff;      // bnlength
+extern bn_t bntmpsqrx, bntmpsqry, bntmp;                        // rlength
+extern BNComplex bnold, bnparm, bnsaved;                        // bnlength
+extern BNComplex bnnew;                                         // rlength
+extern bn_t bn_pi;                                        // TAKES NO SPACE
+extern bf_t bftmp1, bftmp2, bftmp3, bftmp4, bftmp5, bftmp6;  // rbflength+2
+extern bf_t bftmpcpy1, bftmpcpy2;                            // rbflength+2
+extern bf_t bfxdel, bfydel, bfxdel2, bfydel2, bfclosenuff;   // rbflength+2
+extern bf_t bftmpsqrx, bftmpsqry;                            // rbflength+2
+extern BFComplex bfparm, bfsaved;                           // bflength+2
+extern BFComplex bfold,  bfnew;                               // rbflength+2
+extern bf_t bf_pi;                                        // TAKES NO SPACE
+extern bf_t big_pi;                                           // bflength+2
+// for testing only
+// used by other routines
+extern bf_t g_bf_x_min, g_bf_x_max, g_bf_y_min, g_bf_y_max, g_bf_x_3rd, g_bf_y_3rd;   // bflength+2
+extern bf_t g_bf_save_x_min, g_bf_save_x_max, g_bf_save_y_min, g_bf_save_y_max, g_bf_save_x_3rd, g_bf_save_y_3rd;// bflength+2
+extern bf_t bfparms[10];                                 // (bflength+2)*10
+extern bf_t bftmp;
+extern bf_t bf10tmp;                                              // dec+4
 #endif /* BIG_H */
