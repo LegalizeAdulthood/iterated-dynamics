91d1d900f932ca31e09aac90dc0f736ee3737f3a
Richard <legalize@xmission.com>
2018-01-21 23:14:44 -0700

    Refactor: Move WORKLIST from fractint.h to calcfrac.h

--- headers/calcfrac.h
+++ headers/calcfrac.h
@@ -6,6 +6,18 @@
 
 #define MAX_CALC_WORK 12
 
+struct WORKLIST     // work list entry for std escape time engines
+{
+    int xxstart;    // screen window for this entry
+    int xxstop;
+    int yystart;
+    int yystop;
+    int yybegin;    // start row within window, for 2pass/ssg resume
+    int sym;        // if symmetry in window, prevents bad combines
+    int pass;       // for 2pass and solid guessing
+    int xxbegin;    // start col within window, =0 except on resume
+};
+
 extern int                   g_and_color;           // AND mask for iteration to get color index
 extern int                   g_atan_colors;
 extern DComplex              g_attractor[];
--- headers/fractint.h
+++ headers/fractint.h
@@ -926,18 +926,6 @@ enum class jiim_types
     ORBIT
 };
 
-struct WORKLIST     // work list entry for std escape time engines
-{
-    int xxstart;    // screen window for this entry
-    int xxstop;
-    int yystart;
-    int yystop;
-    int yybegin;    // start row within window, for 2pass/ssg resume
-    int sym;        // if symmetry in window, prevents bad combines
-    int pass;       // for 2pass and solid guessing
-    int xxbegin;    // start col within window, =0 except on resume
-};
-
 // for overlay return stack
 #define BIG 100000.0
 
