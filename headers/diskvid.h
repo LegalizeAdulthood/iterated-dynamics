1c769ea433644130d86325f342968d098440974f
Richard <legalize@xmission.com>
2024-02-19 22:22:45 -0700

    Add std::string overload for dvid_status

--- headers/diskvid.h
+++ headers/diskvid.h
@@ -3,6 +3,7 @@
 #define DISKVID_H
 
 #include <cstdio>
+#include <string>
 
 extern bool                  g_disk_16_bit;
 extern bool                  g_disk_flag;       // disk video active flag
@@ -18,6 +19,10 @@ extern void writedisk(int, int, int);
 extern void targa_readdisk(unsigned int, unsigned int, BYTE *, BYTE *, BYTE *);
 extern void targa_writedisk(unsigned int, unsigned int, BYTE, BYTE, BYTE);
 extern void dvid_status(int line, char const *msg);
+inline void dvid_status(int line, const std::string &msg)
+{
+    dvid_status(line, msg.c_str());
+}
 extern int  common_startdisk(long, long, int);
 extern int FromMemDisk(long, int, void *);
 extern bool ToMemDisk(long, int, void *);
