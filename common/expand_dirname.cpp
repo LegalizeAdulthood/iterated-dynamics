13128c89ea11a75265e866ecb4f04e3881d33034
Richard <legalize@xmission.com>
2024-03-02 19:24:18 -0700

    Switch expand_dirname to use std::filesystem

--- common/expand_dirname.cpp
+++ common/expand_dirname.cpp
@@ -1,12 +1,6 @@
 #include "expand_dirname.h"
 
-#include "port.h"
-#include "fractint.h"
-
 #include "fix_dirname.h"
-
-#include <Shlwapi.h>
-#include <crtdbg.h>
 
 #include <cstring>
 #include <filesystem>
@@ -16,27 +10,12 @@ namespace fs = std::filesystem;
 // converts relative path to absolute path
 int expand_dirname(char *dirname, char *drive)
 {
-    if (fs::path(dirname).has_relative_path())
+    fs::path path{dirname};
+    if (path.has_relative_path())
     {
-        char relative[MAX_PATH];
-        _ASSERTE(std::strlen(drive) < NUM_OF(relative));
-        std::strcpy(relative, drive);
-        _ASSERTE(std::strlen(relative) + std::strlen(dirname) < NUM_OF(relative));
-        std::strcat(relative, dirname);
-        char absolute[MAX_PATH];
-        BOOL status = PathSearchAndQualify(relative, absolute, NUM_OF(absolute));
-        _ASSERTE(status);
-        if (absolute[1] == ':')
-        {
-            drive[0] = absolute[0];
-            drive[1] = absolute[1];
-            drive[2] = 0;
-            std::strcpy(dirname, &absolute[2]);
-        }
-        else
-        {
-            std::strcpy(dirname, absolute);
-        }
+        path = absolute(path).make_preferred();
+        std::strcpy(drive, path.root_name().string().c_str());
+        std::strcpy(dirname, path.string().substr(std::strlen(drive)).c_str());
     }
     fix_dirname(dirname);
 
--- tests/test_expand_dirname.cpp
+++ tests/test_expand_dirname.cpp
@@ -1,6 +1,7 @@
 #include <expand_dirname.h>
 
 #include <port.h>
+
 #include <fractint.h>
 
 #include "current_path_saver.h"
@@ -12,17 +13,60 @@
 
 namespace fs = std::filesystem;
 
-TEST(TestExpandDirName, basic)
+namespace
 {
-    char dirname[FILE_MAX_PATH]{};
+
+class TestExpandDirName : public ::testing::Test
+{
+public:
+    ~TestExpandDirName() override = default;
+
+protected:
+    void SetUp() override;
+
+    fs::path m_test_data_dir{ID_TEST_DATA_DIR};
+    fs::path m_test_data_subdir{ID_TEST_DATA_SUBDIR};
+    char m_dir_name[FILE_MAX_PATH]{};
+    char m_drive[FILE_MAX_PATH]{};
+    char m_sep{fs::path::preferred_separator};
+};
+
+void TestExpandDirName::SetUp()
+{
+    Test::SetUp();
+    m_test_data_dir.make_preferred();
+    m_test_data_subdir.make_preferred();
+}
+
+} // namespace
+
+TEST_F(TestExpandDirName, resolvesRelativeDir)
+{
     current_path_saver saver(ID_TEST_DATA_DIR);
-    std::strcpy(dirname, ID_TEST_DATA_SUBDIR_NAME);
-    EXPECT_STREQ("C:", fs::path{ID_TEST_DATA_DIR}.root_name().make_preferred().string().c_str());
-    char drive[FILE_MAX_PATH];
-    std::strcpy(drive, fs::path{ID_TEST_DATA_DIR}.root_name().string().c_str());
+    std::strcpy(m_dir_name, ID_TEST_DATA_SUBDIR_NAME);
+    std::strcpy(m_drive, m_test_data_dir.root_name().string().c_str());
 
-    expand_dirname(dirname, drive);
+    expand_dirname(m_dir_name, m_drive);
 
-    EXPECT_EQ(drive, fs::path{ID_TEST_DATA_SUBDIR}.root_name().make_preferred().string());
-    EXPECT_EQ(dirname, fs::path{ID_TEST_DATA_SUBDIR}.make_preferred().string().substr(2) + static_cast<char>(fs::path::preferred_separator));
+    EXPECT_EQ(m_dir_name, m_test_data_subdir.string().substr(std::strlen(m_drive)) + m_sep);
+}
+
+TEST_F(TestExpandDirName, setsDriveToCurrentDrive)
+{
+    current_path_saver saver(ID_TEST_DATA_DIR);
+    std::strcpy(m_dir_name, ID_TEST_DATA_SUBDIR_NAME);
+
+    expand_dirname(m_dir_name, m_drive);
+
+    EXPECT_EQ(m_drive, m_test_data_subdir.root_name().string());
+}
+
+TEST_F(TestExpandDirName, absolutePathUnchanged)
+{
+    std::strcpy(m_dir_name, m_test_data_dir.string().c_str());
+
+    expand_dirname(m_dir_name, m_drive);
+
+    EXPECT_EQ(m_test_data_dir.root_name().string(), m_drive);
+    EXPECT_EQ(m_test_data_dir.string().substr(std::strlen(m_drive)) + m_sep, m_dir_name);
 }
