c46e8b6f828aa3cac6147912951d428ab39aa321
Richard <legalize@xmission.com>
2024-03-16 20:08:53 -0600

    Change find file tests to be independent of directory iteration order

--- tests/test_find_file.cpp
+++ tests/test_find_file.cpp
@@ -3,32 +3,37 @@
 #include "current_path_saver.h"
 #include "test_data.h"
 
-#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 #include <algorithm>
 #include <filesystem>
+#include <string>
 #include <vector>
 
 namespace fs = std::filesystem;
+
+using namespace testing;
 
 TEST(TestFindFile, firstTextFile)
 {
     const int result = fr_findfirst((fs::path(ID_TEST_DATA_FIND_FILE_DIR) / "*.txt").string().c_str());
 
     EXPECT_EQ(0, result);
-    EXPECT_EQ(ID_TEST_FIND_FILE1, DTA.filename);
+    EXPECT_THAT(DTA.filename, AnyOf(StrEq(ID_TEST_FIND_FILE1), StrEq(ID_TEST_FIND_FILE2)));
     EXPECT_EQ(0, DTA.attribute);
 }
 
 TEST(TestFindFile, secondTextFile)
 {
     fr_findfirst((fs::path(ID_TEST_DATA_FIND_FILE_DIR) / "*.txt").string().c_str());
+    std::vector<std::string> files;
+    files.push_back(DTA.filename);
 
     const int second = fr_findnext();
+    files.push_back(DTA.filename);
 
     EXPECT_EQ(0, second);
-    EXPECT_EQ(ID_TEST_FIND_FILE2, DTA.filename);
-    EXPECT_EQ(0, DTA.attribute);
+    EXPECT_THAT(files, AllOf(Contains(ID_TEST_FIND_FILE1), Contains(ID_TEST_FIND_FILE2)));
 }
 
 TEST(TestFindFile, noMoreTextFiles)
