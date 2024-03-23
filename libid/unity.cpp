4784e1a82c52411c8b5e8ac36c8b3ced114458f9
Richard <legalize@xmission.com>
2024-03-23 14:40:50 -0600

    Prefer stack for temporaries over globals

--- libid/unity.cpp
+++ libid/unity.cpp
@@ -9,25 +9,22 @@
 
 #include <cmath>
 
-static long g_xx_one{};
-
 int UnityFractal()
 {
-    g_xx_one = multiply(g_l_old_z.x, g_l_old_z.x, g_bit_shift) + multiply(g_l_old_z.y, g_l_old_z.y, g_bit_shift);
-    if ((g_xx_one > g_fudge_two) || (labs(g_xx_one - g_fudge_one) < g_l_delta_min))
+    const long xx_one = multiply(g_l_old_z.x, g_l_old_z.x, g_bit_shift) + multiply(g_l_old_z.y, g_l_old_z.y, g_bit_shift);
+    if ((xx_one > g_fudge_two) || (labs(xx_one - g_fudge_one) < g_l_delta_min))
     {
         return 1;
     }
-    g_l_old_z.y = multiply(g_fudge_two - g_xx_one, g_l_old_z.x, g_bit_shift);
-    g_l_old_z.x = multiply(g_fudge_two - g_xx_one, g_l_old_z.y, g_bit_shift);
+    g_l_old_z.y = multiply(g_fudge_two - xx_one, g_l_old_z.x, g_bit_shift);
+    g_l_old_z.x = multiply(g_fudge_two - xx_one, g_l_old_z.y, g_bit_shift);
     g_l_new_z = g_l_old_z;
     return 0;
 }
 
 int UnityfpFractal()
 {
-    double XXOne;
-    XXOne = sqr(g_old_z.x) + sqr(g_old_z.y);
+    const double XXOne = sqr(g_old_z.x) + sqr(g_old_z.y);
     if ((XXOne > 2.0) || (std::fabs(XXOne - 1.0) < g_delta_min))
     {
         return 1;
