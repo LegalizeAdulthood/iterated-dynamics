f38328dd53dc9aa2a54d35255c2f5cb642b6876b
Richard <legalize@xmission.com>
2024-03-26 23:45:23 -0600

    Big-endian decode routines are not unix specific

--- libid/include/unixprot.h
+++ libid/include/unixprot.h
@@ -2,14 +2,6 @@
 
 // This file contains prototypes for unix/linux specific functions.
 
-struct EVOLUTION_INFO;
-struct FRACTAL_INFO;
-struct ORBITS_INFO;
-
-void decode_evolver_info(EVOLUTION_INFO *, int);
-void decode_fractal_info(FRACTAL_INFO *, int);
-void decode_orbits_info(ORBITS_INFO *, int);
-
 int stricmp(char const *, char const *);
 int strnicmp(char const *, char const *, int);
 
