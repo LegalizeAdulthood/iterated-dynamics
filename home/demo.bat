610c0bfb06921078ff3bc7c7457784d41a8d67bb
Richard <legalize@xmission.com>
2024-04-11 12:24:32 -0700

    Update demo.bat to reflect Iterated Dynamics
    Fixes #45

--- home/demo.bat
+++ home/demo.bat
@@ -1,4 +1,5 @@
 @echo off
+rem TODO: Re-enable sound demo when audio library is selected.
 :top
 cls
 echo ************************************************************
@@ -12,23 +13,23 @@ echo ************************************************************
 echo.
 echo 0:  Exit
 echo 1:  Basic commands demo
-echo 2:  New stuff in version 19
-echo 3:  New stuff in version 19.4
-echo 4:  New stuff in version 19.5
-echo 5:  New stuff in version 19.6
+echo 2:  Bailout tests, browser, outside=atan, ants
+echo 3:  Rounding functions, showdot turtle
+echo 4:  recordcolors=, comment= parameters
+echo 5:  fastrestore parameter, text scrolling in ^<Z^> screen
 echo 6:  Advanced commands demo
 echo 7:  Evolver demo
 echo 9:  All demos
 set /p "choice=Your choice? "
 echo.
 if "%choice%" == "9" goto all
-if "%choice%" == "8" goto sound
+rem if "%choice%" == "8" goto sound
 if "%choice%" == "7" goto evolver
 if "%choice%" == "6" goto advanced
-if "%choice%" == "5" goto newin196
-if "%choice%" == "4" goto newin195
-if "%choice%" == "3" goto newin194
-if "%choice%" == "2" goto newin19
+if "%choice%" == "5" goto demo4
+if "%choice%" == "4" goto demo3
+if "%choice%" == "3" goto demo2
+if "%choice%" == "2" goto demo1
 if "%choice%" == "1" goto basic
 if "%choice%" == "0" goto end
 goto top
@@ -47,55 +48,62 @@ goto top
 id savename=.\ filename=.\ curdir=yes @demo.par/Mandel_Demo autokey=play autokeyname=advanced.key
 goto top
 
-:newin19
-id savename=.\ filename=.\ curdir=yes @demo.par/Mandel_Demo autokey=play autokeyname=new19.key
-del new19???.gif
+
+:demo1
+id savename=.\ filename=.\ curdir=yes @demo.par/Mandel_Demo autokey=play autokeyname=demo1.key
+del demo1*.gif
 goto top
 
-:newin194
-id savename=.\ filename=.\ curdir=yes @demo.par/trunc_Demo autokey=play autokeyname=new19-4.key
-del new1940?.gif
+
+:demo2
+id savename=.\ filename=.\ curdir=yes @demo.par/trunc_Demo autokey=play autokeyname=demo2.key
+del demo2*.gif
 goto top
 
-:newin195
-set r=n195b
-goto n195s
-:n195b
-del demo195.par
+
+:demo3
+set r=demo3b
+goto demo3s
+:demo3b
+del demo3.par
 goto top
 
-:newin196
-id savename=.\ filename=.\ curdir=yes autokey=play autokeyname=new19-6.key
-del demo196?.gif
+
+:demo4
+id savename=.\ filename=.\ curdir=yes autokey=play autokeyname=demo4.key
+del demo4*.gif
 goto top
+
 
 :basic
 id savename=.\ filename=.\ curdir=yes @demo.par/Mandel_Demo autokey=play autokeyname=basic.key
 del basic001.gif
 goto top
 
+
 :all
 id savename=.\ filename=.\ curdir=yes @demo.par/Mandel_Demo autokey=play autokeyname=basic.key
-id savename=.\ filename=.\ curdir=yes @demo.par/Mandel_Demo autokey=play autokeyname=new19.key
-id savename=.\ filename=.\ curdir=yes @demo.par/trunc_Demo autokey=play autokeyname=new19-4.key
+id savename=.\ filename=.\ curdir=yes @demo.par/Mandel_Demo autokey=play autokeyname=demo1.key
+id savename=.\ filename=.\ curdir=yes @demo.par/trunc_Demo autokey=play autokeyname=demo2.key
 set r=allb
-goto n195s
+goto demo3s
 :allb
-id savename=.\ filename=.\ curdir=yes autokey=play autokeyname=new19-6.key
+id savename=.\ filename=.\ curdir=yes autokey=play autokeyname=demo4.key
 id savename=.\ filename=.\ curdir=yes @demo.par/Mandel_Demo autokey=play autokeyname=advanced.key
 del basic001.gif
-del new19???.gif
-del new1940?.gif
-del demo195.par
-del demo196?.gif
-id savename=.\ filename=.\ curdir=yes autokey=play autokeyname=snddemo1.key
+del demo1*.gif
+del demo2*.gif
+del demo3.par
+del demo4*.gif
+rem id savename=.\ filename=.\ curdir=yes autokey=play autokeyname=snddemo1.key
 id savename=.\ filename=.\ curdir=yes autokey=play autokeyname=explore.key
 goto top
 
-:n195s
+
+:demo3s
 cls
 echo.
-echo   This autokey demonstrates the new "comment=" and "colors=" commands
+echo   This autokey demonstrates the "comment=" and "colors=" parameters
 echo   ===================================================================
 echo.
 echo   It will draw the classic Mandel fractal, then:
@@ -104,18 +112,17 @@ echo   - load default.map
 echo   - enter "recordcolors=comment"
 echo           The compressed palette will be written in the par entry and
 echo           the .map file will be in a comment
-echo   - enter "comments=$date$/$xdots$_x_$ydots$/On_a_Pentium_166/$calctime$
+echo   - enter "comments=$date$/$xdots$_x_$ydots$/Calculation_time:_$calctime$
 echo           These comments will be expanded as follow in the par:
-echo           Mandel_demo { ; Aug 26, 1996
-echo                         ; 160 x 100
-echo"                        ; On a Pentium 166
-echo                         ;  0:00:00.07
+echo           Mandel_demo { ; May 20, 2024
+echo                         ; 800 x 600
+echo                         ; Calculation time: 0:00:00.07
 echo   - make a par called "Mandel_Demo"
 echo   - load this par and let you see with F2 the complete par entry
 echo   - exit
 echo.
 pause
-id savename=.\ filename=.\ curdir=yes @demo.par/Mandel_Demo autokey=play autokeyname=new19-5.key
+id savename=.\ filename=.\ curdir=yes @demo.par/Mandel_Demo autokey=play autokeyname=demo3.key
 goto %r%
 
 :end
