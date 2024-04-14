dfd17dee7415d81b336e440328329132aed0fcab
Richard <legalize@xmission.com>
2024-04-14 01:56:22 -0700

    Create Windows and Unix variants of trim filename test

--- tests/test_trim_filename.cpp
+++ tests/test_trim_filename.cpp
@@ -2,6 +2,10 @@
 
 #include <gtest/gtest.h>
 
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
 TEST(TestTrimFilename, fitsInRequestedSize)
 {
     const std::string filename{R"(C:\iterated-dynamics\foo.par)"};
@@ -11,7 +15,8 @@ TEST(TestTrimFilename, fitsInRequestedSize)
     EXPECT_EQ(filename, result);
 }
 
-TEST(TestTrimFilename, dropsIntermediateDirectories)
+#if defined(WIN32)
+TEST(TestTrimFilename, dropsIntermediateDirectoriesWindows)
 {
     const std::string filename{R"(C:\code\iterated-dynamics\build-default\install\pars\foo.par)"};
     
@@ -21,3 +26,15 @@ TEST(TestTrimFilename, dropsIntermediateDirectories)
     EXPECT_GE(44U, result.size());
     EXPECT_EQ(R"(C:\code\...\install\pars\foo.par)", result);
 }
+#else
+TEST(TestTrimFilename, dropsIntermediateDirectoriesUnix)
+{
+    const std::string filename{R"(/home/users/l/legalize/iterated-dynamics/build-default/install/pars/foo.par)"};
+    
+    const std::string result = trim_filename(filename, 44);
+
+               //12345678901234567890123456789012345678901234
+    EXPECT_GE(44U, result.size());
+    EXPECT_EQ(R"(/home/.../install/pars/foo.par)", result);
+}
+#endif
