9566748f2bea531653831a23faabffdc7469d98c
Richard <legalize@xmission.com>
2024-05-05 20:28:01 -0600

    Localize scroll_file_start

--- libid/full_screen_prompt.cpp
+++ libid/full_screen_prompt.cpp
@@ -55,7 +55,6 @@ int fullscreen_prompt(        // full-screen prompting routine
 )
 {
     // scrolling related variables
-    long scroll_file_start = 0;    // where entry starts in scroll_file
     bool in_scrolling_mode = false; // will be true if need to scroll extrainfo
     int lines_in_entry = 0;        // total lines in entry to be scrolled
     int vertical_scroll_limit = 0; // don't scroll down if this is top line
@@ -74,6 +73,7 @@ int fullscreen_prompt(        // full-screen prompting routine
        beginning of the entry.
     */
     std::FILE *scroll_file = nullptr; // file with extrainfo entry to scroll
+    long scroll_file_start = 0;       // where entry starts in scroll_file
     if (extra_info && *extra_info)
     {
         if (g_fractal_type == fractal_type::FORMULA || g_fractal_type == fractal_type::FFORMULA)
