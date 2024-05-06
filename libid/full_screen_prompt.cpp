cbac611a3f957c6616c3a34efdf008a73fd7760b
Richard <legalize@xmission.com>
2024-05-05 20:05:17 -0600

    Localize boxlines

--- libid/full_screen_prompt.cpp
+++ libid/full_screen_prompt.cpp
@@ -54,7 +54,6 @@ int fullscreen_prompt(        // full-screen prompting routine
     char *extra_info          // extra info box to display, \n separated
 )
 {
-    int boxlines;
     int boxcol;
     int boxwidth;
     int extralines;
@@ -235,6 +234,7 @@ int fullscreen_prompt(        // full-screen prompting routine
 
     // work out vertical positioning
     int titlerow;
+    int boxlines;
     {
         int i = num_prompts + titlelines + extralines + 3; // total rows required
         int j = (25 - i) / 2;                   // top row of it all when centered
