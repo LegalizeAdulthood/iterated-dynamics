3b1fd0c9c30bfed9ddb8b6e3ae5560413abb1b59
Richard <legalize@xmission.com>
2024-07-06 17:19:34 -0400

    Use ValueSaver in get_commands

--- libid/get_commands.cpp
+++ libid/get_commands.cpp
@@ -7,6 +7,7 @@
 #include "file_item.h"
 #include "helpdefs.h"
 #include "id_data.h"
+#include "value_saver.h"
 
 #include <cstdio>
 
@@ -15,14 +16,12 @@ cmdarg_flags get_commands()
 {
     std::FILE *parmfile;
     cmdarg_flags ret{cmdarg_flags::NONE};
-    help_labels const old_help_mode = g_help_mode;
-    g_help_mode = help_labels::HELP_PARMFILE;
+    ValueSaver saved_help_mode{g_help_mode, help_labels::HELP_PARMFILE};
     long point = get_file_entry(gfe_type::PARM, "Parameter Set", "*.par", g_command_file, g_command_name);
     if (point >= 0 && (parmfile = std::fopen(g_command_file.c_str(), "rb")) != nullptr)
     {
         std::fseek(parmfile, point, SEEK_SET);
         ret = load_commands(parmfile);
     }
-    g_help_mode = old_help_mode;
     return ret;
 }
