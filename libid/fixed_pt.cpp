17c9ca7d67f02c4e277187ac4c94e76f64077d56
Richard <legalize@xmission.com>
2024-07-09 18:39:04 -0400

    Uniform initialization of global data in fixed_pt.cpp

--- libid/fixed_pt.cpp
+++ libid/fixed_pt.cpp
@@ -1,5 +1,5 @@
 #include "fixed_pt.h"
 
-int g_bit_shift;                       // fudgefactor
-long g_fudge_factor;                   // 2**fudgefactor
+int g_bit_shift{};     // fudgefactor
+long g_fudge_factor{}; // 2**fudgefactor
 bool g_overflow{};
