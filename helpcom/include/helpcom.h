07226e259812719740fc9cd0db5e0cc4c940b0b2
Richard <legalize@xmission.com>
2024-07-10 11:22:37 -0400

    Replace Macro With Enum in helpcom.h

--- helpcom/include/helpcom.h
+++ helpcom/include/helpcom.h
@@ -28,28 +28,29 @@ enum help_commands
     CMD_SPACE = 8,      // next byte is count of spaces
     MAX_CMD = 8
 };
-/*
- * on-line help dimensions
- */
-#define SCREEN_WIDTH      (78)
-#define SCREEN_DEPTH      (22)
-#define SCREEN_INDENT     (1)
-/*
- * printed document dimensions
- */
-#define PAGE_WIDTH         (72)  // width of printed text
-#define PAGE_INDENT        (2)   // indent all text by this much
-#define TITLE_INDENT       (1)   // indent titles by this much
-#define PAGE_RDEPTH        (59)  // the total depth (inc. heading)
-#define PAGE_HEADING_DEPTH (3)   // depth of the heading
-#define PAGE_DEPTH         (PAGE_RDEPTH-PAGE_HEADING_DEPTH) // depth of text
-/*
- * Document page-break macros.  Goto to next page if this close (or closer)
- * to end of page when starting a CONTENT, TOPIC, or at a BLANK line.
- */
-#define CONTENT_BREAK (7)  // start of a "DocContent" entry
-#define TOPIC_BREAK   (4)  // start of each topic under a DocContent entry
-#define BLANK_BREAK   (2)  // a blank line
+
+enum
+{
+    // on-line help dimensions
+    SCREEN_WIDTH = 78,
+    SCREEN_DEPTH = 22,
+    SCREEN_INDENT = 1,
+    
+    // printed document dimensions
+    PAGE_WIDTH = 72,                               // width of printed text
+    PAGE_INDENT = 2,                               // indent all text by this much
+    TITLE_INDENT = 1,                              // indent titles by this much
+    PAGE_RDEPTH = 59,                              // the total depth (inc. heading)
+    PAGE_HEADING_DEPTH = 3,                        // depth of the heading
+    PAGE_DEPTH = PAGE_RDEPTH - PAGE_HEADING_DEPTH, // depth of text
+    
+    // Document page-break values.  Goto to next page if this close (or closer)
+    // to end of page when starting a CONTENT, TOPIC, or at a BLANK line.
+    CONTENT_BREAK = 7, // start of a "DocContent" entry
+    TOPIC_BREAK = 4,   // start of each topic under a DocContent entry
+    BLANK_BREAK = 2    // a blank line
+};
+
 /*
  * tokens returned by find_token_length
  */
@@ -66,6 +67,7 @@ enum class token_types
     TOK_XDOC = 8,       // a CMD_XDOC
     TOK_CENTER = 9      // a CMD_CENTER
 };
+
 /*
  * modes for find_token_length() and find_line_width()
  */
@@ -75,6 +77,7 @@ enum class token_modes
     ONLINE = 1,
     DOC = 2
 };
+
 /*
  * struct PD_INFO used by process_document()
  */
@@ -94,6 +97,7 @@ struct PD_INFO
     char const *s;
     int i;
 };
+
 /*
  * Commands passed to (*get_info)() and (*output)() by process_document()
  */
@@ -116,6 +120,7 @@ enum class PD_COMMANDS
     PD_RELEASE_TOPIC,
     PD_GET_LINK_PAGE
 };
+
 typedef bool (*PD_FUNC)(PD_COMMANDS cmd, PD_INFO *pd, void *info);
 token_types find_token_length(token_modes mode, char const *curr, unsigned len, int *size, int *width);
 int find_line_width(token_modes mode, char const *curr, unsigned len);
