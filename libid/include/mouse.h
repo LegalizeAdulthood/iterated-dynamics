713ad59a48ef69e74dad51d7806e46452fd6e755
Richard <legalize@xmission.com>
2024-09-13 18:59:16 -0600

    Improve g_look_at_mouse documentation

--- libid/include/mouse.h
+++ libid/include/mouse.h
@@ -7,9 +7,10 @@ extern int                   g_look_at_mouse;
 // or it is set to a negative key code for primary button click to generate a key.
 enum class MouseLook
 {
-    IGNORE = 0,      // ignoring the mouse
-    TEXT_SCROLL = 2, // for scrolling through text lists
-    POSITION = 3,    // for positioning things like the zoom box, palette editor, etc.
+    IGNORE = 0,            // ignoring the mouse
+    NAVIGATE_GRAPHICS = 1, // <Return> + arrow keys at graphics cursor grid
+    NAVIGATE_TEXT = 2,     // <Return> + arrow keys at text cursor grid
+    POSITION = 3,          // for positioning things like the zoom box, palette editor, etc.
 };
 inline int operator+(MouseLook value)
 {
--- libid/mouse.cpp
+++ libid/mouse.cpp
@@ -1,4 +1,14 @@
 #include "mouse.h"
 
-int g_look_at_mouse{};                                        //
-bool g_cursor_mouse_tracking{};
+// g_look_at_mouse:
+//     0  ignore the mouse entirely
+//    <0  only test for left button click; if it occurs return fake key number of opposite sign
+//     1  return <Enter> key for left button, arrow keys for mouse movement,
+//        mouse sensitivity is suitable for graphics cursor
+//     2  same as 1 but sensitivity is suitable for text cursor
+//     3  specials for zoom box, left/right double-clicks generate fake
+//        keys, mouse movement generates a variety of fake keys
+//        depending on state of buttons
+//
+int g_look_at_mouse{};          //
+bool g_cursor_mouse_tracking{}; //
