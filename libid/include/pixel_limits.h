b952fae6d78c17aa4eed61cda08bfdbca2b4250e
Richard <legalize@xmission.com>
2024-11-25 23:13:00 -0500

    Convert Macro To Enum in pixel_limits.h

--- libid/include/pixel_limits.h
+++ libid/include/pixel_limits.h
@@ -2,6 +2,9 @@
 //
 #pragma once
 
-#define MAX_PIXELS   32767          // Maximum pixel count across/down the screen
-#define OLD_MAX_PIXELS 2048         // Limit of some old fixed arrays
-#define MIN_PIXELS 10               // Minimum pixel count across/down the screen
+enum
+{
+    MAX_PIXELS = 32767,    // Maximum pixel count across/down the screen
+    OLD_MAX_PIXELS = 2048, // Limit of some old fixed arrays
+    MIN_PIXELS = 10        // Minimum pixel count across/down the screen
+};
