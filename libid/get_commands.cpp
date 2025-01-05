27bb691dd0f78b01822713f4992c306a704f8d52
Richard <legalize@xmission.com>
2025-01-05 16:32:47 -0700

    Rename parm to param in get_commands.cpp

--- libid/get_commands.cpp
+++ libid/get_commands.cpp
@@ -12,14 +12,14 @@
 // execute commands from file
 CmdArgFlags get_commands()
 {
-    std::FILE *parmfile;
+    std::FILE *param_file;
     CmdArgFlags ret{CmdArgFlags::NONE};
     ValueSaver saved_help_mode{g_help_mode, HelpLabels::HELP_PARAM_FILE};
     long point = get_file_entry(ItemType::PARM, "Parameter Set", "*.par", g_command_file, g_command_name);
-    if (point >= 0 && (parmfile = std::fopen(g_command_file.c_str(), "rb")) != nullptr)
+    if (point >= 0 && (param_file = std::fopen(g_command_file.c_str(), "rb")) != nullptr)
     {
-        std::fseek(parmfile, point, SEEK_SET);
-        ret = load_commands(parmfile);
+        std::fseek(param_file, point, SEEK_SET);
+        ret = load_commands(param_file);
     }
     return ret;
 }
