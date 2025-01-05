ee0ea667c3aabb8ba2e3f7807fbdcfcdacc70f56
Richard <legalize@xmission.com>
2025-01-04 20:20:57 -0700

    Join Declaration and Assignment in circle_pattern.cpp

--- libid/circle_pattern.cpp
+++ libid/circle_pattern.cpp
@@ -8,8 +8,7 @@
 
 int circle_fp_fractal()
 {
-    long i;
-    i = (long)(g_params[0]*(g_temp_sqr_x+g_temp_sqr_y));
+    long i = (long) (g_params[0] * (g_temp_sqr_x + g_temp_sqr_y));
     g_color_iter = i%g_colors;
     return 1;
 }
