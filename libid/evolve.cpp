783d46275a7f3ffcd56cd9c4a459e753564b3970
Richard <legalize@xmission.com>
2025-01-04 22:06:15 -0700

    Prefer vector::data over address of first element in evolve.cpp

--- libid/evolve.cpp
+++ libid/evolve.cpp
@@ -959,18 +959,18 @@ void draw_param_box(int mode)
     if (g_box_count)
     {
         // stash normal zoombox pixels
-        std::copy(&g_box_x[0], &g_box_x[g_box_count*2], &s_image_box_x[0]);
-        std::copy(&g_box_y[0], &g_box_y[g_box_count*2], &s_image_box_y[0]);
-        std::copy(&g_box_values[0], &g_box_values[g_box_count], &s_image_box_values[0]);
+        std::copy(&g_box_x[0], &g_box_x[g_box_count*2], s_image_box_x.data());
+        std::copy(&g_box_y[0], &g_box_y[g_box_count*2], s_image_box_y.data());
+        std::copy(&g_box_values[0], &g_box_values[g_box_count], s_image_box_values.data());
         clear_box(); // to avoid probs when one box overlaps the other
     }
     if (g_evolve_param_box_count != 0)
     {
         // clear last parmbox
         g_box_count = g_evolve_param_box_count;
-        std::copy(&s_param_box_x[0], &s_param_box_x[g_box_count*2], &g_box_x[0]);
-        std::copy(&s_param_box_y[0], &s_param_box_y[g_box_count*2], &g_box_y[0]);
-        std::copy(&s_param_box_values[0], &s_param_box_values[g_box_count], &g_box_values[0]);
+        std::copy(s_param_box_x.data(), &s_param_box_x[g_box_count*2], &g_box_x[0]);
+        std::copy(s_param_box_y.data(), &s_param_box_y[g_box_count*2], &g_box_y[0]);
+        std::copy(s_param_box_values.data(), &s_param_box_values[g_box_count], &g_box_values[0]);
         clear_box();
     }
 
@@ -1001,18 +1001,18 @@ void draw_param_box(int mode)
     {
         display_box();
         // stash pixel values for later
-        std::copy(&g_box_x[0], &g_box_x[g_box_count*2], &s_param_box_x[0]);
-        std::copy(&g_box_y[0], &g_box_y[g_box_count*2], &s_param_box_y[0]);
-        std::copy(&g_box_values[0], &g_box_values[g_box_count], &s_param_box_values[0]);
+        std::copy(&g_box_x[0], &g_box_x[g_box_count*2], s_param_box_x.data());
+        std::copy(&g_box_y[0], &g_box_y[g_box_count*2], s_param_box_y.data());
+        std::copy(&g_box_values[0], &g_box_values[g_box_count], s_param_box_values.data());
     }
     g_evolve_param_box_count = g_box_count;
     g_box_count = s_image_box_count;
     if (s_image_box_count)
     {
         // and move back old values so that everything can proceed as normal
-        std::copy(&s_image_box_x[0], &s_image_box_x[g_box_count*2], &g_box_x[0]);
-        std::copy(&s_image_box_y[0], &s_image_box_y[g_box_count*2], &g_box_y[0]);
-        std::copy(&s_image_box_values[0], &s_image_box_values[g_box_count], &g_box_values[0]);
+        std::copy(s_image_box_x.data(), &s_image_box_x[g_box_count*2], &g_box_x[0]);
+        std::copy(s_image_box_y.data(), &s_image_box_y[g_box_count*2], &g_box_y[0]);
+        std::copy(s_image_box_values.data(), &s_image_box_values[g_box_count], &g_box_values[0]);
         display_box();
     }
 }
