453b840523d3fbcca902f17e1f2ed24d17b8d45c
Richard <legalize@xmission.com>
2025-01-06 01:25:41 -0700

    Rename variables to all_lower_case in ChoiceBuilder.h

--- libid/include/ChoiceBuilder.h
+++ libid/include/ChoiceBuilder.h
@@ -11,10 +11,10 @@
 
 struct FullScreenPrompt
 {
-    int operator()(char const *hdg, int numprompts, char const **prompts, FullScreenValues *values, int fkeymask,
-        char *extrainfo) const
+    int operator()(char const *hdg, int num_prompts, char const **prompts, FullScreenValues *values,
+        int fn_key_mask, char *extra_info) const
     {
-        return full_screen_prompt(hdg, numprompts, prompts, values, fkeymask, extrainfo);
+        return full_screen_prompt(hdg, num_prompts, prompts, values, fn_key_mask, extra_info);
     }
 };
 
@@ -26,83 +26,83 @@ public:
     {
         check_build_overflow();
         choices[m_current_build] = choice;
-        uvalues[m_current_build].type = 'y';
-        uvalues[m_current_build].uval.ch.val = value ? 1 : 0;
+        values[m_current_build].type = 'y';
+        values[m_current_build].uval.ch.val = value ? 1 : 0;
         return advance();
     }
     ChoiceBuilder &int_number(const char *choice, int value)
     {
         check_build_overflow();
         choices[m_current_build] = choice;
-        uvalues[m_current_build].type = 'i';
-        uvalues[m_current_build].uval.ival = value;
+        values[m_current_build].type = 'i';
+        values[m_current_build].uval.ival = value;
         return advance();
     }
     ChoiceBuilder &long_number(const char *choice, long value)
     {
         check_build_overflow();
         choices[m_current_build] = choice;
-        uvalues[m_current_build].type = 'L';
-        uvalues[m_current_build].uval.Lval = value;
+        values[m_current_build].type = 'L';
+        values[m_current_build].uval.Lval = value;
         return advance();
     }
     ChoiceBuilder &float_number(const char *choice, float value)
     {
         check_build_overflow();
         choices[m_current_build] = choice;
-        uvalues[m_current_build].type = 'f';
-        uvalues[m_current_build].uval.dval = value;
+        values[m_current_build].type = 'f';
+        values[m_current_build].uval.dval = value;
         return advance();
     }
     ChoiceBuilder &double_number(const char *choice, double value)
     {
         check_build_overflow();
         choices[m_current_build] = choice;
-        uvalues[m_current_build].type = 'd';
-        uvalues[m_current_build].uval.dval = value;
+        values[m_current_build].type = 'd';
+        values[m_current_build].uval.dval = value;
         return advance();
     }
     ChoiceBuilder &int_double_number(const char *choice, double value)
     {
         check_build_overflow();
         choices[m_current_build] = choice;
-        uvalues[m_current_build].type = 'D';
-        uvalues[m_current_build].uval.dval = value;
+        values[m_current_build].type = 'D';
+        values[m_current_build].uval.dval = value;
         return advance();
     }
     ChoiceBuilder &comment(const char *choice)
     {
         check_build_overflow();
         choices[m_current_build] = choice;
-        uvalues[m_current_build].type = '*';
+        values[m_current_build].type = '*';
         return advance();
     }
     ChoiceBuilder &list(const char *choice, int list_len, int list_value_len, const char *list[], int value)
     {
         check_build_overflow();
         choices[m_current_build] = choice;
-        uvalues[m_current_build].type = 'l';
-        uvalues[m_current_build].uval.ch.llen = list_len;
-        uvalues[m_current_build].uval.ch.vlen = list_value_len;
-        uvalues[m_current_build].uval.ch.list = list;
-        uvalues[m_current_build].uval.ch.val = value;
+        values[m_current_build].type = 'l';
+        values[m_current_build].uval.ch.llen = list_len;
+        values[m_current_build].uval.ch.vlen = list_value_len;
+        values[m_current_build].uval.ch.list = list;
+        values[m_current_build].uval.ch.val = value;
         return advance();
     }
     ChoiceBuilder &string(const char *choice, const char *value)
     {
         check_build_overflow();
         choices[m_current_build] = choice;
-        uvalues[m_current_build].type = 's';
-        std::strncpy(uvalues[m_current_build].uval.sval, value, 16);
-        uvalues[m_current_build].uval.sval[15] = 0;
+        values[m_current_build].type = 's';
+        std::strncpy(values[m_current_build].uval.sval, value, 16);
+        values[m_current_build].uval.sval[15] = 0;
         return advance();
     }
     ChoiceBuilder &string_buff(const char *choice, char *buffer, int len)
     {
         check_build_overflow();
         choices[m_current_build] = choice;
-        uvalues[m_current_build].type = 0x100 + len;
-        uvalues[m_current_build].uval.sbuf = buffer;
+        values[m_current_build].type = 0x100 + len;
+        values[m_current_build].uval.sbuf = buffer;
         return advance();
     }
 
@@ -111,7 +111,7 @@ public:
         m_current_build = 0;
         m_current_read = 0;
         choices = std::array<const char *, N>{};
-        uvalues = std::array<FullScreenValues, N>{};
+        values = std::array<FullScreenValues, N>{};
         m_prompted = false;
         m_result = 0;
         return *this;
@@ -122,16 +122,16 @@ public:
         return m_current_build;
     }
 
-    int prompt(char const *hdg, int fkeymask, char *extrainfo)
+    int prompt(char const *hdg, int fn_key_mask, char *extra_info)
     {
         m_prompted = true;
-        m_result = m_prompter(hdg, count(), choices.data(), uvalues.data(), fkeymask, extrainfo);
+        m_result = m_prompter(hdg, count(), choices.data(), values.data(), fn_key_mask, extra_info);
         m_current_read = 0;
         return m_result;
     }
-    int prompt(char const *hdg, int fkeymask)
+    int prompt(char const *hdg, int fn_key_mask)
     {
-        return prompt(hdg, fkeymask, nullptr);
+        return prompt(hdg, fn_key_mask, nullptr);
     }
     int prompt(char const *hdg)
     {
@@ -141,32 +141,32 @@ public:
     bool read_yes_no() const
     {
         check_read_type("yes/no", 'l');
-        return uvalues[m_current_read++].uval.ch.val == 1;
+        return values[m_current_read++].uval.ch.val == 1;
     }
     int read_int_number() const
     {
         check_read_type("int", 'i');
-        return uvalues[m_current_read++].uval.ival;
+        return values[m_current_read++].uval.ival;
     }
     long read_long_number() const
     {
         check_read_type("long", 'L');
-        return uvalues[m_current_read++].uval.Lval;
+        return values[m_current_read++].uval.Lval;
     }
     float read_float_number() const
     {
         check_read_type("float", 'f');
-        return static_cast<float>(uvalues[m_current_read++].uval.dval);
+        return static_cast<float>(values[m_current_read++].uval.dval);
     }
     double read_double_number() const
     {
         check_read_type("double", 'd');
-        return uvalues[m_current_read++].uval.dval;
+        return values[m_current_read++].uval.dval;
     }
     double read_int_double_number() const
     {
         check_read_type("int double", 'D');
-        return uvalues[m_current_read++].uval.dval;
+        return values[m_current_read++].uval.dval;
     }
     void read_comment() const
     {
@@ -176,22 +176,22 @@ public:
     int read_list() const
     {
         check_read_type("list", 'l');
-        return uvalues[m_current_read++].uval.ch.val;
+        return values[m_current_read++].uval.ch.val;
     }
     const char *read_string() const
     {
         check_read_type("string", 's');
-        return uvalues[m_current_read++].uval.sval;
+        return values[m_current_read++].uval.sval;
     }
     const char *read_string_buff(int len) const
     {
         check_read_type("string buff", 0x100 + len);
-        return uvalues[m_current_read++].uval.sbuf;
+        return values[m_current_read++].uval.sbuf;
     }
 
 private:
     std::array<const char *, N> choices{};
-    std::array<FullScreenValues, N> uvalues{};
+    std::array<FullScreenValues, N> values{};
 
     ChoiceBuilder &advance()
     {
@@ -216,10 +216,10 @@ private:
             throw std::runtime_error("Reading result before being prompted for result");
         }
         check_overflow(m_current_read);
-        if (uvalues[m_current_read].type != type)
+        if (values[m_current_read].type != type)
         {
             throw std::runtime_error((std::string{"Attempt to read "} + label + " value from '" +
-                static_cast<char>(uvalues[m_current_read].type) + "' type")
+                static_cast<char>(values[m_current_read].type) + "' type")
                                          .c_str());
         }
     }
