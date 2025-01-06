6a98f691be127ec0a0214364fd3f3b2ab7c357b9
Richard <legalize@xmission.com>
2025-01-06 00:30:57 -0700

    Rename variables to all_lower_case in get_toggles2.cpp

--- libid/get_toggles2.cpp
+++ libid/get_toggles2.cpp
@@ -22,45 +22,45 @@
 int get_toggles2()
 {
     char const *choices[18];
-    FullScreenValues uvalues[23];
-    double old_potparam[3];
+    FullScreenValues values[23];
+    double old_potential_param[3];
     double old_inversion[3];
 
     // fill up the choices (and previous values) arrays
     int k = -1;
 
     choices[++k] = "Look for finite attractor (0=no,>0=yes,<0=phase)";
-    uvalues[k].type = 'i';
-    uvalues[k].uval.ch.val = g_finite_attractor ? 1 : 0;
+    values[k].type = 'i';
+    values[k].uval.ch.val = g_finite_attractor ? 1 : 0;
 
     choices[++k] = "Potential Max Color (0 means off)";
-    uvalues[k].type = 'i';
-    old_potparam[0] = g_potential_params[0];
-    uvalues[k].uval.ival = (int) old_potparam[0];
+    values[k].type = 'i';
+    old_potential_param[0] = g_potential_params[0];
+    values[k].uval.ival = (int) old_potential_param[0];
 
     choices[++k] = "          Slope";
-    uvalues[k].type = 'd';
-    old_potparam[1] = g_potential_params[1];
-    uvalues[k].uval.dval = old_potparam[1];
+    values[k].type = 'd';
+    old_potential_param[1] = g_potential_params[1];
+    values[k].uval.dval = old_potential_param[1];
 
     choices[++k] = "          Bailout";
-    uvalues[k].type = 'i';
-    old_potparam[2] = g_potential_params[2];
-    uvalues[k].uval.ival = (int) old_potparam[2];
+    values[k].type = 'i';
+    old_potential_param[2] = g_potential_params[2];
+    values[k].uval.ival = (int) old_potential_param[2];
 
     choices[++k] = "          16 bit values";
-    uvalues[k].type = 'y';
-    uvalues[k].uval.ch.val = g_potential_16bit ? 1 : 0;
+    values[k].type = 'y';
+    values[k].uval.ch.val = g_potential_16bit ? 1 : 0;
 
     choices[++k] = "Distance Estimator (0=off, <0=edge, >0=on):";
-    uvalues[k].type = 'L';
-    long old_usr_distest = g_user_distance_estimator_value;
-    uvalues[k].uval.Lval = old_usr_distest;
+    values[k].type = 'L';
+    long old_user_distance_estimator_value = g_user_distance_estimator_value;
+    values[k].uval.Lval = old_user_distance_estimator_value;
 
     choices[++k] = "          width factor:";
-    uvalues[k].type = 'i';
-    int old_distestwidth = g_distance_estimator_width_factor;
-    uvalues[k].uval.ival = old_distestwidth;
+    values[k].type = 'i';
+    int old_distance_estimator_width_factor = g_distance_estimator_width_factor;
+    values[k].uval.ival = old_distance_estimator_width_factor;
 
     choices[++k] = "Inversion radius or \"auto\" (0 means off)";
     choices[++k] = "          center X coordinate or \"auto\"";
@@ -68,39 +68,39 @@ int get_toggles2()
     k = k - 3;
     for (int i = 0; i < 3; i++)
     {
-        uvalues[++k].type = 's';
+        values[++k].type = 's';
         old_inversion[i] = g_inversion[i];
         if (g_inversion[i] == AUTO_INVERT)
         {
-            std::sprintf(uvalues[k].uval.sval, "auto");
+            std::sprintf(values[k].uval.sval, "auto");
         }
         else
         {
             char buff[80]{};
             std::sprintf(buff, "%-1.15lg", g_inversion[i]);
-            buff[std::size(uvalues[k].uval.sval)-1] = 0;
-            std::strcpy(uvalues[k].uval.sval, buff);
+            buff[std::size(values[k].uval.sval)-1] = 0;
+            std::strcpy(values[k].uval.sval, buff);
         }
     }
     choices[++k] = "  (use fixed radius & center when zooming)";
-    uvalues[k].type = '*';
+    values[k].type = '*';
 
     choices[++k] = "Color cycling from color (0 ... 254)";
-    uvalues[k].type = 'i';
+    values[k].type = 'i';
     int old_rotate_lo = g_color_cycle_range_lo;
-    uvalues[k].uval.ival = old_rotate_lo;
+    values[k].uval.ival = old_rotate_lo;
 
     choices[++k] = "              to   color (1 ... 255)";
-    uvalues[k].type = 'i';
+    values[k].type = 'i';
     int old_rotate_hi = g_color_cycle_range_hi;
-    uvalues[k].uval.ival = old_rotate_hi;
+    values[k].uval.ival = old_rotate_hi;
 
     HelpLabels const old_help_mode = g_help_mode;
     g_help_mode = HelpLabels::HELP_Y_OPTIONS;
     {
         int i = full_screen_prompt("Extended Options\n"
                               "(not all combinations make sense)",
-                              k+1, choices, uvalues, 0, nullptr);
+                              k+1, choices, values, 0, nullptr);
         g_help_mode = old_help_mode;
         if (i < 0)
         {
@@ -112,33 +112,33 @@ int get_toggles2()
     k = -1;
     bool changed = false;
 
-    if ((uvalues[++k].uval.ch.val != 0) != g_finite_attractor)
+    if ((values[++k].uval.ch.val != 0) != g_finite_attractor)
     {
-        g_finite_attractor = uvalues[k].uval.ch.val != 0;
+        g_finite_attractor = values[k].uval.ch.val != 0;
         changed = true;
     }
 
-    g_potential_params[0] = uvalues[++k].uval.ival;
-    if (g_potential_params[0] != old_potparam[0])
-    {
-        changed = true;
-    }
-
-    g_potential_params[1] = uvalues[++k].uval.dval;
-    if (g_potential_params[0] != 0.0 && g_potential_params[1] != old_potparam[1])
+    g_potential_params[0] = values[++k].uval.ival;
+    if (g_potential_params[0] != old_potential_param[0])
     {
         changed = true;
     }
 
-    g_potential_params[2] = uvalues[++k].uval.ival;
-    if (g_potential_params[0] != 0.0 && g_potential_params[2] != old_potparam[2])
+    g_potential_params[1] = values[++k].uval.dval;
+    if (g_potential_params[0] != 0.0 && g_potential_params[1] != old_potential_param[1])
     {
         changed = true;
     }
 
-    if ((uvalues[++k].uval.ch.val != 0) != g_potential_16bit)
+    g_potential_params[2] = values[++k].uval.ival;
+    if (g_potential_params[0] != 0.0 && g_potential_params[2] != old_potential_param[2])
     {
-        g_potential_16bit = uvalues[k].uval.ch.val != 0;
+        changed = true;
+    }
+
+    if ((values[++k].uval.ch.val != 0) != g_potential_16bit)
+    {
+        g_potential_16bit = values[k].uval.ch.val != 0;
         if (g_potential_16bit)                   // turned it on
         {
             if (g_potential_params[0] != 0.0)
@@ -160,27 +160,27 @@ int get_toggles2()
     }
 
     ++k;
-    g_user_distance_estimator_value = uvalues[k].uval.Lval;
-    if (g_user_distance_estimator_value != old_usr_distest)
+    g_user_distance_estimator_value = values[k].uval.Lval;
+    if (g_user_distance_estimator_value != old_user_distance_estimator_value)
     {
         changed = true;
     }
     ++k;
-    g_distance_estimator_width_factor = uvalues[k].uval.ival;
-    if (g_user_distance_estimator_value && g_distance_estimator_width_factor != old_distestwidth)
+    g_distance_estimator_width_factor = values[k].uval.ival;
+    if (g_user_distance_estimator_value && g_distance_estimator_width_factor != old_distance_estimator_width_factor)
     {
         changed = true;
     }
 
     for (int i = 0; i < 3; i++)
     {
-        if (uvalues[++k].uval.sval[0] == 'a' || uvalues[k].uval.sval[0] == 'A')
+        if (values[++k].uval.sval[0] == 'a' || values[k].uval.sval[0] == 'A')
         {
             g_inversion[i] = AUTO_INVERT;
         }
         else
         {
-            g_inversion[i] = std::atof(uvalues[k].uval.sval);
+            g_inversion[i] = std::atof(values[k].uval.sval);
         }
         if (old_inversion[i] != g_inversion[i]
             && (i == 0 || g_inversion[0] != 0.0))
@@ -191,8 +191,8 @@ int get_toggles2()
     g_invert = (g_inversion[0] == 0.0) ? 0 : 3;
     ++k;
 
-    g_color_cycle_range_lo = uvalues[++k].uval.ival;
-    g_color_cycle_range_hi = uvalues[++k].uval.ival;
+    g_color_cycle_range_lo = values[++k].uval.ival;
+    g_color_cycle_range_hi = values[++k].uval.ival;
     if (g_color_cycle_range_lo < 0 || g_color_cycle_range_hi > 255 || g_color_cycle_range_lo > g_color_cycle_range_hi)
     {
         g_color_cycle_range_lo = old_rotate_lo;
