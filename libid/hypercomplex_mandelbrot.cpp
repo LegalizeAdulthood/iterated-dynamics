75fdab517e9d1143282da563d556e6870475dd6a
Richard <legalize@xmission.com>
2025-01-06 00:55:17 -0700

    Rename variables to all_lower_case in hypercomplex_mandelbrot.cpp

--- libid/hypercomplex_mandelbrot.cpp
+++ libid/hypercomplex_mandelbrot.cpp
@@ -9,26 +9,26 @@
 
 int hyper_complex_fp_fractal()
 {
-    DHyperComplex hold;
-    DHyperComplex hnew;
-    hold.x = g_old_z.x;
-    hold.y = g_old_z.y;
-    hold.z = g_float_param->x;
-    hold.t = g_float_param->y;
+    DHyperComplex h_old;
+    DHyperComplex h_new;
+    h_old.x = g_old_z.x;
+    h_old.y = g_old_z.y;
+    h_old.z = g_float_param->x;
+    h_old.t = g_float_param->y;
 
-    hcmplx_trig0(&hold, &hnew);
+    hcmplx_trig0(&h_old, &h_new);
 
-    hnew.x += g_quaternion_c;
-    hnew.y += g_quaternion_ci;
-    hnew.z += g_quaternion_cj;
-    hnew.t += g_quaternion_ck;
+    h_new.x += g_quaternion_c;
+    h_new.y += g_quaternion_ci;
+    h_new.z += g_quaternion_cj;
+    h_new.t += g_quaternion_ck;
 
-    g_new_z.x = hnew.x;
+    g_new_z.x = h_new.x;
     g_old_z.x = g_new_z.x;
-    g_new_z.y = hnew.y;
+    g_new_z.y = h_new.y;
     g_old_z.y = g_new_z.y;
-    g_float_param->x = hnew.z;
-    g_float_param->y = hnew.t;
+    g_float_param->x = h_new.z;
+    g_float_param->y = h_new.t;
 
     // Check bailout
     g_magnitude = sqr(g_old_z.x)+sqr(g_old_z.y)+sqr(g_float_param->x)+sqr(g_float_param->y);
