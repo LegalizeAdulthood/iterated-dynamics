abf231b173d248511637182f533d4f12fc00402e
Richard <legalize@xmission.com>
2025-01-05 23:27:26 -0700

    Rename variables to all_lower_case in escher.cpp

--- libid/escher.cpp
+++ libid/escher.cpp
@@ -11,29 +11,29 @@
 
 int escher_fp_fractal() // Science of Fractal Images pp. 185, 187
 {
-    DComplex oldtest;
-    DComplex testsqr;
-    double testsize = 0.0;
-    long testiter = 0;
+    DComplex old_test;
+    DComplex test_sqr;
+    double test_size = 0.0;
+    long test_iter = 0;
 
     g_new_z.x = g_temp_sqr_x - g_temp_sqr_y; // standard Julia with C == (0.0, 0.0i)
     g_new_z.y = 2.0 * g_old_z.x * g_old_z.y;
-    oldtest.x = g_new_z.x * 15.0;    // scale it
-    oldtest.y = g_new_z.y * 15.0;
-    testsqr.x = sqr(oldtest.x);  // set up to test with user-specified ...
-    testsqr.y = sqr(oldtest.y);  //    ... Julia as the target set
-    while (testsize <= g_magnitude_limit && testiter < g_max_iterations) // nested Julia loop
+    old_test.x = g_new_z.x * 15.0;    // scale it
+    old_test.y = g_new_z.y * 15.0;
+    test_sqr.x = sqr(old_test.x);  // set up to test with user-specified ...
+    test_sqr.y = sqr(old_test.y);  //    ... Julia as the target set
+    while (test_size <= g_magnitude_limit && test_iter < g_max_iterations) // nested Julia loop
     {
-        DComplex newtest;
-        newtest.x = testsqr.x - testsqr.y + g_params[0];
-        newtest.y = 2.0 * oldtest.x * oldtest.y + g_params[1];
-        testsqr.x = sqr(newtest.x);
-        testsqr.y = sqr(newtest.y);
-        testsize = testsqr.x + testsqr.y;
-        oldtest = newtest;
-        testiter++;
+        DComplex new_test;
+        new_test.x = test_sqr.x - test_sqr.y + g_params[0];
+        new_test.y = 2.0 * old_test.x * old_test.y + g_params[1];
+        test_sqr.x = sqr(new_test.x);
+        test_sqr.y = sqr(new_test.y);
+        test_size = test_sqr.x + test_sqr.y;
+        old_test = new_test;
+        test_iter++;
     }
-    if (testsize > g_magnitude_limit)
+    if (test_size > g_magnitude_limit)
     {
         return g_bailout_float(); // point not in target set
     }
