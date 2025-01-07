6cb9f08d5d76c6cca3b4aa4171a1b7c809505843
Richard <legalize@xmission.com>
2025-01-07 01:27:14 -0700

    Rename variables to all_lower_case in passes_options.cpp

--- libid/passes_options.cpp
+++ libid/passes_options.cpp
@@ -21,12 +21,12 @@
 int passes_options()
 {
     char const *choices[20];
-    char const *passcalcmodes[] = {"rect", "line"};
+    char const *pass_calc_modes[] = {"rect", "line"};
 
-    FullScreenValues uvalues[25];
+    FullScreenValues values[25];
     int i;
 
-    bool const old_keep_scrn_coords = g_keep_screen_coords;
+    bool const old_keep_screen_coords = g_keep_screen_coords;
 
     int ret = 0;
 
@@ -35,41 +35,41 @@ pass_option_restart:
     int k = -1;
 
     choices[++k] = "Periodicity (0=off, <0=show, >0=on, -255..+255)";
-    uvalues[k].type = 'i';
+    values[k].type = 'i';
     int old_periodicity = g_user_periodicity_value;
-    uvalues[k].uval.ival = old_periodicity;
+    values[k].uval.ival = old_periodicity;
 
     choices[++k] = "Orbit delay (0 = none)";
-    uvalues[k].type = 'i';
+    values[k].type = 'i';
     int old_orbit_delay = g_orbit_delay;
-    uvalues[k].uval.ival = old_orbit_delay;
+    values[k].uval.ival = old_orbit_delay;
 
     choices[++k] = "Orbit interval (1 ... 255)";
-    uvalues[k].type = 'i';
+    values[k].type = 'i';
     int old_orbit_interval = (int) g_orbit_interval;
-    uvalues[k].uval.ival = old_orbit_interval;
+    values[k].uval.ival = old_orbit_interval;
 
     choices[++k] = "Maintain screen coordinates";
-    uvalues[k].type = 'y';
-    uvalues[k].uval.ch.val = g_keep_screen_coords ? 1 : 0;
+    values[k].type = 'y';
+    values[k].uval.ch.val = g_keep_screen_coords ? 1 : 0;
 
     choices[++k] = "Orbit pass shape (rect, line)";
     //   choices[++k] = "Orbit pass shape (rect,line,func)";
-    uvalues[k].type = 'l';
-    uvalues[k].uval.ch.vlen = 5;
-    uvalues[k].uval.ch.list_len = sizeof(passcalcmodes)/sizeof(*passcalcmodes);
-    uvalues[k].uval.ch.list = passcalcmodes;
-    uvalues[k].uval.ch.val = (g_draw_mode == 'r') ? 0
+    values[k].type = 'l';
+    values[k].uval.ch.vlen = 5;
+    values[k].uval.ch.list_len = sizeof(pass_calc_modes)/sizeof(*pass_calc_modes);
+    values[k].uval.ch.list = pass_calc_modes;
+    values[k].uval.ch.val = (g_draw_mode == 'r') ? 0
                              : (g_draw_mode == 'l') ? 1
                              :   /* function */    2;
-    char old_drawmode = g_draw_mode;
+    char old_draw_mode = g_draw_mode;
 
     {
         ValueSaver saved_help_mode{g_help_mode, HelpLabels::HELP_PASSES_OPTIONS};
         i = full_screen_prompt("Passes Options\n"
                               "(not all combinations make sense)\n"
                               "(Press F2 for corner parameters)\n"
-                              "(Press F6 for calculation parameters)", k+1, choices, uvalues, 64 | 4, nullptr);
+                              "(Press F6 for calculation parameters)", k+1, choices, values, 64 | 4, nullptr);
     }
     if (i < 0)
     {
@@ -80,7 +80,7 @@ pass_option_restart:
     k = -1;
     int j = 0;   // return code
 
-    g_user_periodicity_value = uvalues[++k].uval.ival;
+    g_user_periodicity_value = values[++k].uval.ival;
     g_user_periodicity_value = std::min(g_user_periodicity_value, 255);
     g_user_periodicity_value = std::max(g_user_periodicity_value, -255);
     if (g_user_periodicity_value != old_periodicity)
@@ -88,13 +88,13 @@ pass_option_restart:
         j = 1;
     }
 
-    g_orbit_delay = uvalues[++k].uval.ival;
+    g_orbit_delay = values[++k].uval.ival;
     if (g_orbit_delay != old_orbit_delay)
     {
         j = 1;
     }
 
-    g_orbit_interval = uvalues[++k].uval.ival;
+    g_orbit_interval = values[++k].uval.ival;
     g_orbit_interval = std::min(g_orbit_interval, 255L);
     g_orbit_interval = std::max(g_orbit_interval, 1L);
     if (g_orbit_interval != old_orbit_interval)
@@ -102,8 +102,8 @@ pass_option_restart:
         j = 1;
     }
 
-    g_keep_screen_coords = uvalues[++k].uval.ch.val != 0;
-    if (g_keep_screen_coords != old_keep_scrn_coords)
+    g_keep_screen_coords = values[++k].uval.ch.val != 0;
+    if (g_keep_screen_coords != old_keep_screen_coords)
     {
         j = 1;
     }
@@ -113,7 +113,7 @@ pass_option_restart:
     }
 
     {
-        switch (uvalues[++k].uval.ch.val)
+        switch (values[++k].uval.ch.val)
         {
         default:
         case 0:
@@ -127,7 +127,7 @@ pass_option_restart:
             break;
         }
     }
-    if (g_draw_mode != old_drawmode)
+    if (g_draw_mode != old_draw_mode)
     {
         j = 1;
     }
