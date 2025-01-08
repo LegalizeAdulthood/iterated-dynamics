cadf79e568c2f5cd9c25d4decd9ad8c090b30bd8
Richard <legalize@xmission.com>
2025-01-07 18:01:32 -0700

    Rename variables to all_lower_case in ssg_block_size.cpp

--- libid/ssg_block_size.cpp
+++ libid/ssg_block_size.cpp
@@ -7,17 +7,17 @@
 int ssg_block_size() // used by solidguessing and by zoom panning
 {
     // blocksize 4 if <300 rows, 8 if 300-599, 16 if 600-1199, 32 if >=1200
-    int blocksize = 4;
+    int block_size = 4;
     int i = 300;
     while (i <= g_logical_screen_y_dots)
     {
-        blocksize += blocksize;
+        block_size += block_size;
         i += i;
     }
     // increase blocksize if prefix array not big enough
-    while (blocksize*(MAX_X_BLK-2) < g_logical_screen_x_dots || blocksize*(MAX_Y_BLK-2)*16 < g_logical_screen_y_dots)
+    while (block_size*(MAX_X_BLK-2) < g_logical_screen_x_dots || block_size*(MAX_Y_BLK-2)*16 < g_logical_screen_y_dots)
     {
-        blocksize += blocksize;
+        block_size += block_size;
     }
-    return blocksize;
+    return block_size;
 }
