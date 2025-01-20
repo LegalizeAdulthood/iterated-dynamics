ad0e45fa138df98c9e1d38d51d996a3e6d17571c
Richard <legalize@xmission.com>
2025-01-19 19:14:57 -0700

    Rename local variables to all_lower_case in mpmath.cpp

--- libid/math/mpmath.cpp
+++ libid/math/mpmath.cpp
@@ -155,8 +155,8 @@ long exp_float14(long xx)
 {
     static float fLogTwo = 0.6931472F;
     int f = 23 - (int) reg_float_to_fg(reg_div_float(xx, *(long *) &fLogTwo), 0);
-    long Ans = exp_fudged(reg_float_to_fg(xx, 16), f);
-    return reg_fg_to_float(Ans, (char)f);
+    long ans = exp_fudged(reg_float_to_fg(xx, 16), f);
+    return reg_fg_to_float(ans, (char)f);
 }
 
 /*
