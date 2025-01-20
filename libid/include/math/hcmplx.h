5f944430cf30c25335e1d9ae07e33f535120413a
Richard <legalize@xmission.com>
2025-01-20 10:27:10 -0700

    Use parameter names in function declarations in hcmplx.h

--- libid/include/math/hcmplx.h
+++ libid/include/math/hcmplx.h
@@ -25,4 +25,4 @@ int hcmplx_inv(DHyperComplex *arg, DHyperComplex *out);
 void hcmplx_add(DHyperComplex *arg1, DHyperComplex *arg2, DHyperComplex *out);
 void hcmplx_sub(DHyperComplex *arg1, DHyperComplex *arg2, DHyperComplex *out);
 void hcmplx_minus(DHyperComplex *arg1, DHyperComplex *out);
-void hcmplx_trig0(DHyperComplex *, DHyperComplex *);
+void hcmplx_trig0(DHyperComplex *h, DHyperComplex *out);
