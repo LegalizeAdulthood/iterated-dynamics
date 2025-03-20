1e26ccde731e78c76050e2f19736e8620f656910
Paul the LionHeart <pdeleeuw@deleeuw.com.au>
2025-03-20 16:28:53 +1100

    A few cleanups and the beginning of designing a new reference process

--- libid/engine/PertEngine.cpp
+++ libid/engine/PertEngine.cpp
@@ -44,6 +44,8 @@ void PertEngine::initialize_frame(
         m_center_bf.y = alloc_stack(g_bf_length + 2);
         copy_bf(m_center_bf.x, center_bf.x);
         copy_bf(m_center_bf.y, center_bf.y);
+        m_c_bf.x = alloc_stack(g_r_bf_length + 2);
+        m_c_bf.y = alloc_stack(g_r_bf_length + 2);
     }
     else
     {
@@ -60,26 +62,11 @@ int PertEngine::calculate_one_frame()
     std::complex<double> reference_coordinate;
 
     m_reference_points = 0;
-    m_glitch_point_count = 0L;
-    m_remaining_point_count = 0L;
-
-    m_points_remaining.resize(g_screen_x_dots * g_screen_y_dots);
-    m_glitch_points.resize(g_screen_x_dots * g_screen_y_dots);
     m_perturbation_tolerance_check.resize(g_max_iterations * 2);
     m_xn.resize(g_max_iterations + 1);
 
     // calculate the pascal's triangle coefficients for powers > 3
     pascal_triangle();
-    // Fill the list of points with all points in the image.
-    for (long y = 0; y < g_screen_y_dots; y++)
-    {
-        for (long x = 0; x < g_screen_x_dots; x++)
-        {
-            Point pt(x, g_screen_y_dots - 1 - y);
-            m_points_remaining[y * g_screen_x_dots + x] = pt;
-            m_remaining_point_count++;
-        }
-    }
     
     double magnified_radius = m_zoom_radius;
     int window_radius = std::min(g_screen_x_dots, g_screen_y_dots);
@@ -95,124 +82,35 @@ int PertEngine::calculate_one_frame()
         tmp_bf = alloc_stack(g_r_bf_length + 2);
     }
 
-    if (m_remaining_point_count > (g_screen_x_dots * g_screen_y_dots) * (m_percent_glitch_tolerance / 100))
-    {
-        m_reference_points++;
+    m_reference_points++;
 
         // Determine the reference point to calculate
         // Check whether this is the first time running the loop.
-        if (m_reference_points == 1)
-        {
-            if (g_bf_math != BFMathType::NONE)
-            {
-                copy_bf(c_bf.x, m_center_bf.x);
-                copy_bf(c_bf.y, m_center_bf.y);
-                copy_bf(reference_coordinate_bf.x, c_bf.x);
-                copy_bf(reference_coordinate_bf.y, c_bf.y);
-            }
-            else
-            {
-                c = m_center;
-                reference_coordinate = m_center;
-            }
-
-            m_delta_real = 0;
-            m_delta_imag = 0;
-
-            if (g_bf_math != BFMathType::NONE)
-            {
-                reference_zoom_point(reference_coordinate_bf, g_max_iterations);
-            }
-            else
-            {
-                reference_zoom_point(reference_coordinate, g_max_iterations);
-            }
-        }
-        else
-        {
-            restore_stack(m_saved_stack);
-            return 0;
-        }
+    if (g_bf_math != BFMathType::NONE)
+    {
+        copy_bf(c_bf.x, m_center_bf.x);
+        copy_bf(c_bf.y, m_center_bf.y);
+        copy_bf(reference_coordinate_bf.x, c_bf.x);
+        copy_bf(reference_coordinate_bf.y, c_bf.y);
     }
-        /*
-        {
-            if (!m_calculate_glitches)
-            {
-                break;
-            }
-
-            std::srand(g_random_seed);
-            if (!g_random_seed_flag)
-            {
-                ++g_random_seed;
-            }
-            const int index{(int) ((double) std::rand() / RAND_MAX * m_remaining_point_count)};
-            Point pt{m_points_remaining[index]};
-            // Get the complex point at the chosen reference point
-            double delta_real = magnified_radius * (2 * pt.get_x() - g_screen_x_dots) / window_radius;
-            double delta_imag = -magnified_radius * (2 * pt.get_y() - g_screen_y_dots) / window_radius;
-
-            // We need to store this offset because the formula we use to convert pixels into a complex point
-            // does so relative to the center of the image. We need to offset that calculation when our
-            // reference point isn't in the center. The actual offsetting is done in calculate point.
-
-            m_delta_real = delta_real;
-            m_delta_imag = delta_imag;
-
-            if (g_bf_math != BFMathType::NONE)
-            {
-                float_to_bf(tmp_bf, delta_real);
-                add_bf(reference_coordinate_bf.x, c_bf.x, tmp_bf);
-                float_to_bf(tmp_bf, delta_imag);
-                add_bf(reference_coordinate_bf.y, c_bf.y, tmp_bf);
-            }
-            else
-            {
-                reference_coordinate.real(c.real() + delta_real);
-                reference_coordinate.imag(c.imag() + delta_imag);
-            }
-
-            if (g_bf_math != BFMathType::NONE)
-            {
-                reference_zoom_point(reference_coordinate_bf, g_max_iterations);
-            }
-            else
-            {
-                reference_zoom_point(reference_coordinate, g_max_iterations);
-            }
-        }
-
-        int last_checked{-1};
-        m_glitch_point_count = 0;
-        for (int i = 0; i < m_remaining_point_count; i++)
-        {
-            if (i % 1000 == 0 && driver_key_pressed())
-            {
-                return -1;
-            }
-            Point pt{m_points_remaining[i]};
-            if (calculate_point(pt, magnified_radius, window_radius) < 0)
-            {
-                return -1;
-            }
-            // Everything else in this loop is just for updating the progress counter.
-            double progress = (double) i / m_remaining_point_count;
-            if (int(progress * 100) != last_checked)
-            {
-                last_checked = int(progress * 100);
-                m_status = "Pass: " + std::to_string(m_reference_points) + ", Ref (" +
-                    std::to_string(int(progress * 100)) + "%)";
-            }
-        }
-
-        // These points are glitched, so we need to mark them for recalculation. We need to recalculate them
-        // using Pauldelbrot's glitch fixing method (see calculate point).
-        std::memcpy(m_points_remaining.data(), m_glitch_points.data(), sizeof(Point) * m_glitch_point_count);
-        m_remaining_point_count = m_glitch_point_count;
+    else
+    {
+        c = m_center;
+        reference_coordinate = m_center;
     }
 
-    cleanup();
-*/
+    m_delta_real = 0;
+    m_delta_imag = 0;
+
+    if (g_bf_math != BFMathType::NONE)
+    {
+        reference_zoom_point(reference_coordinate_bf, g_max_iterations);
+    }
+    else
+    {
+        reference_zoom_point(reference_coordinate, g_max_iterations);
+    }
+        restore_stack(m_saved_stack);
     return 0;
 }
 
@@ -222,237 +120,9 @@ void PertEngine::cleanup()
     {
         restore_stack(m_saved_stack);
     }
-//    m_points_remaining.clear();
-//    m_glitch_points.clear();
     m_perturbation_tolerance_check.clear();
     m_xn.clear();
 }
-
-#ifdef OLDPOINT
-
-int PertEngine::calculate_point(const Point &pt, double magnified_radius, int window_radius)
-{
-    // Get the complex number at this pixel.
-    // This calculates the number relative to the reference point, so we need to translate that to the center
-    // when the reference point isn't in the center. That's why for the first reference,
-    // m_calculated_real_delta and m_calculated_imaginary_delta are 0: it's calculating relative to the
-    // center.
-    const double delta_real =
-        ((magnified_radius * (2 * pt.get_x() - g_screen_x_dots)) / window_radius) - m_delta_real;
-    const double delta_imaginary =
-        ((-magnified_radius * (2 * pt.get_y() - g_screen_y_dots)) / window_radius) - m_delta_imag;
-    /*std::complex<double> */m_delta_sub_0 = {delta_real, delta_imaginary};
-    /*std::complex<double> */m_delta_sub_n = {delta_real, delta_imaginary};
-    int iteration{};
-    bool glitched{};
-
-    double min_orbit{1e5}; // orbit value closest to origin
-    long min_index{};      // iteration of min_orbit
-    double magnitude;
-    do
-    {
-        if (g_cur_fractal_specific->pert_pt == nullptr)
-        {
-            throw std::runtime_error("No perturbation point function defined for fractal type (" +
-                std::string{g_cur_fractal_specific->name} + ")");
-        }
-        g_cur_fractal_specific->pert_pt(m_xn[iteration], m_delta_sub_n, m_delta_sub_0);
-        iteration++;
-        magnitude = mag_squared(m_xn[iteration] + m_delta_sub_n);
-
-        if (g_inside_color == BOF60 || g_inside_color == BOF61)
-        {
-            if (magnitude < min_orbit)
-            {
-                min_orbit = magnitude;
-                min_index = iteration + 1L;
-            }
-        }
-
-        // This is Pauldelbrot's glitch detection method. You can see it here:
-        // http://www.fractalforums.com/announcements-and-news/pertubation-theory-glitches-improvement/. As
-        // for why it looks so weird, it's because I've squared both sides of his equation and moved the
-        // |ZsubN| to the other side to be precalculated. For more information, look at where the reference
-        // point is calculated. I also only want to store this point once.
-        if (m_calculate_glitches && !glitched &&
-            magnitude < m_perturbation_tolerance_check[iteration])
-        {
-            m_glitch_points[m_glitch_point_count] = Point(pt.get_x(), pt.get_y(), iteration);
-            m_glitch_point_count++;
-            glitched = true;
-            break;
-        }
-    } while (magnitude < g_magnitude_limit && iteration < g_max_iterations);
-
-    if (!glitched)
-    {
-        int index;
-        const double rq_lim2{std::sqrt(g_magnitude_limit)};
-        const std::complex<double> w{m_xn[iteration] + m_delta_sub_n};
-
-        if (g_biomorph >= 0)
-        {
-            if (iteration == g_max_iterations)
-            {
-                index = g_max_iterations;
-            }
-            else
-            {
-                if (std::abs(w.real()) < rq_lim2 || std::abs(w.imag()) < rq_lim2)
-                {
-                    index = g_biomorph;
-                }
-                else
-                {
-                    index = iteration % 256;
-                }
-            }
-        }
-        else
-        {
-            switch (g_outside_color)
-            {
-            case 0: // no filter
-                if (iteration == g_max_iterations)
-                {
-                    index = g_max_iterations;
-                }
-                else
-                {
-                    index = iteration % 256;
-                }
-                break;
-
-            case ZMAG:
-                if (iteration == g_max_iterations)
-                {
-                    index = (int) ((w.real() * w.real() + w.imag() + w.imag()) * (g_max_iterations >> 1) + 1);
-                }
-                else
-                {
-                    index = iteration % 256;
-                }
-                break;
-
-            case REAL:
-                if (iteration == g_max_iterations)
-                {
-                    index = g_max_iterations;
-                }
-                else
-                {
-                    index = iteration + (long) w.real() + 7;
-                }
-                break;
-
-            case IMAG:
-                if (iteration == g_max_iterations)
-                {
-                    index = g_max_iterations;
-                }
-                else
-                {
-                    index = iteration + (long) w.imag() + 7;
-                }
-                break;
-
-            case MULT:
-                if (iteration == g_max_iterations)
-                {
-                    index = g_max_iterations;
-                }
-                else if (w.imag())
-                {
-                    index = (long) ((double) iteration * (w.real() / w.imag()));
-                }
-                else
-                {
-                    index = iteration;
-                }
-                break;
-
-            case SUM:
-                if (iteration == g_max_iterations)
-                {
-                    index = g_max_iterations;
-                }
-                else
-                {
-                    index = iteration + (long) (w.real() + w.imag());
-                }
-                break;
-
-            case ATAN:
-                if (iteration == g_max_iterations)
-                {
-                    index = g_max_iterations;
-                }
-                else
-                {
-                    index = (long) std::abs(atan2(w.imag(), w.real()) * 180.0 / PI);
-                }
-                break;
-
-            default:
-                if (g_potential_flag)
-                {
-                    index = potential(mag_squared(w), iteration);
-                }
-                else // no filter
-                {
-                    if (iteration == g_max_iterations)
-                    {
-                        index = g_max_iterations;
-                    }
-                    else
-                    {
-                        index = iteration % 256;
-                    }
-                }
-                break;
-            }
-
-            if (g_inside_color >= 0) // no filter
-            {
-                if (iteration == g_max_iterations)
-                {
-                    index = g_inside_color;
-                }
-                else
-                {
-                    index = iteration % 256;
-                }
-            }
-            else
-            {
-                switch (g_inside_color)
-                {
-                case ZMAG:
-                    if (iteration == g_max_iterations)
-                    {
-                        index = (int) (mag_squared(w) * (g_max_iterations >> 1) + 1);
-                    }
-                    break;
-                case BOF60:
-                    if (iteration == g_max_iterations)
-                    {
-                        index = (int) (std::sqrt(min_orbit) * 75.0);
-                    }
-                    break;
-                case BOF61:
-                    if (iteration == g_max_iterations)
-                    {
-                        index = min_index;
-                    }
-                    break;
-                }
-            }
-        }
-        g_plot(pt.get_x(), g_screen_y_dots - 1 - pt.get_y(), index);
-    }
-    return 0;
-}
-#endif  // OLDPOINT
 
 void PertEngine::reference_zoom_point(const BFComplex &center, int max_iteration)
 {
@@ -578,7 +248,6 @@ int PertEngine::calculate_orbit(int x, int y, long iteration)
             std::string{g_cur_fractal_specific->name} + ")");
     }
     temp = m_xn[iteration] + m_delta_sub_n;
-//    m_glitches[x + y * g_screen_x_dots] = 0; // assume not glitched
     magnitude = mag_squared(temp);
 
     // This is Pauldelbrot's glitch detection method. You can see it here:
@@ -586,16 +255,12 @@ int PertEngine::calculate_orbit(int x, int y, long iteration)
     // why it looks so weird, it's because I've squared both sides of his equation and moved the |ZsubN| to
     // the other side to be precalculated. For more information, look at where the reference point is
     // calculated. I also only want to store this point once.
-    if (/*m_calculate_glitches && !m_glitched && */magnitude < m_perturbation_tolerance_check[iteration])
+    if (magnitude < m_perturbation_tolerance_check[iteration])
     {
     // here is where the magic happens... eventually
-    /*
-//        m_glitches[x + y * g_screen_x_dots] = 1; // yep, she's glitched
-        Point pt(x, y, iteration);
-        m_glitch_points[m_glitch_point_count] = pt;
-        m_glitch_point_count++;
-//        m_glitched = true;
-    */
+    //    calculate_reference(x, y);
+    //    calculate_orbit(x, y, iteration);
+    //    return g_bailout_float();
         return true;
     }
     g_new_z.x = temp.real();
@@ -612,4 +277,77 @@ int PertEngine::calculate_orbit(int x, int y, long iteration)
         float_to_bf(g_new_z_bf.y, temp.imag());
     }
     return g_bailout_float();
+}
+
+int PertEngine::calculate_reference(int x, int y)
+{
+    double magnified_radius = m_zoom_radius;
+    int window_radius = std::min(g_screen_x_dots, g_screen_y_dots);
+    BFComplex reference_coordinate_bf{};
+    BigFloat tmp_bf{};
+    std::complex<double> reference_coordinate;
+    int saved = save_stack();
+
+//    if (m_calculate_glitches == false)
+//        return 0;
+
+    if (g_bf_math != BFMathType::NONE)
+    {
+        reference_coordinate_bf.x = alloc_stack(g_r_bf_length + 2);
+        reference_coordinate_bf.y = alloc_stack(g_r_bf_length + 2);
+        tmp_bf = alloc_stack(g_r_bf_length + 2);
+    }
+/*
+    std::srand(g_random_seed);
+    if (!g_random_seed_flag)
+    {
+        ++g_random_seed;
+    }
+
+    m_reference_points++;
+
+    const int index{(int) ((double) std::rand() / RAND_MAX * m_remaining_point_count)};
+    Point pt{m_points_remaining[index]};
+*/
+    // Get the complex point at the chosen reference point
+//    double delta_real = magnified_radius * (2 * pt.get_x() - g_screen_x_dots) / window_radius;
+//    double delta_imag = -magnified_radius * (2 * pt.get_y() - g_screen_y_dots) / window_radius;
+    double delta_real = magnified_radius * (2 * x - g_screen_x_dots) / window_radius;
+    double delta_imag = -magnified_radius * (2 * y - g_screen_y_dots) / window_radius;
+
+    // We need to store this offset because the formula we use to convert pixels into a complex point
+    // does so relative to the center of the image. We need to offset that calculation when our
+    // reference point isn't in the center. The actual offsetting is done in calculate point.
+
+    m_delta_real = delta_real;
+    m_delta_imag = delta_imag;
+
+    if (g_bf_math != BFMathType::NONE)
+    {
+        float_to_bf(tmp_bf, delta_real);
+        add_bf(reference_coordinate_bf.x, m_c_bf.x, tmp_bf);
+        float_to_bf(tmp_bf, delta_imag);
+        sub_bf(reference_coordinate_bf.y, m_c_bf.y, tmp_bf);
+    }
+    else
+    {
+        reference_coordinate.real(m_c.real() + delta_real);
+        reference_coordinate.imag(m_c.imag() - delta_imag);
+    }
+
+    if (g_bf_math != BFMathType::NONE)
+    {
+        reference_zoom_point(reference_coordinate_bf, g_max_iterations);
+    }
+    else
+    {
+        reference_zoom_point(reference_coordinate, g_max_iterations);
+    }
+
+    if (g_bf_math != BFMathType::NONE)
+    {
+        restore_stack(saved);
+    }
+
+    return 0;
 }
--- libid/engine/fractalb.cpp
+++ libid/engine/fractalb.cpp
@@ -356,16 +356,7 @@ bool mandel_bn_setup()
             half_a_bn(g_close_enough_bn);
         }
     }
-/*
-    if (g_use_perturbation && bit_set(g_cur_fractal_specific->flags, FractalFlags::PERTURB))
-    {
-        mandel_perturbation_setup();
-        // TODO: figure out crash if we don't do this
-//        g_std_calc_mode ='g';
-//        g_calc_status = CalcStatus::COMPLETED;
-        return true;
-    }
-*/
+
     g_c_exponent = (int) g_params[2];
     switch (g_fractal_type)
     {
@@ -476,12 +467,6 @@ bool mandel_bf_setup()
     {
     case FractalType::MANDEL:
     case FractalType::BURNING_SHIP:
-/*
-        if (g_use_perturbation && bit_set(g_cur_fractal_specific->flags, FractalFlags::PERTURB))
-        {
-            return mandel_perturbation_setup();
-        }
-*/
         break;
 
     case FractalType::JULIA:
@@ -490,20 +475,6 @@ bool mandel_bf_setup()
         break;
 
     case FractalType::MANDEL_Z_POWER:
-/*
-        if (g_use_perturbation && bit_set(g_cur_fractal_specific->flags, FractalFlags::PERTURB))
-        {
-            // only allow integer values of real part
-            if (const int degree = (int) g_params[2]; degree > 2)
-            {
-                return mandel_z_power_perturbation_setup();
-            }
-            else if (degree == 2)
-            {
-                return mandel_perturbation_setup();
-            }
-        }
-*/
         init_big_pi();
         if ((double) g_c_exponent == g_params[2] && (g_c_exponent & 1)) // odd exponents
         {
