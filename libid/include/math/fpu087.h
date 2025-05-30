2a69b3b522298fbad0e0b3318be01c6612c87fca
Richard <legalize@xmission.com>
2025-05-30 16:09:48 -0600

    Drop unused integer math routines from fpu087.{h,cpp}

--- libid/include/math/fpu087.h
+++ libid/include/math/fpu087.h
@@ -12,14 +12,3 @@ void fpu_cmplx_log(const DComplex *x, DComplex *z);                    // z = lo
 void fpu_cmplx_exp(const DComplex *x, DComplex *z);                    // z = e^x
 void sin_cos(const double *angle, double *sine, double *cosine);
 void sinh_cosh(const double *angle, double *sine, double *cosine);
-
-void sin_cos(long angle, long *sine, long *cosine);
-void sinh_cosh(long angle, long *sine, long *cosine);
-long r16_mul(long x, long y);
-long reg_float_to_fg(long x, int fudge);
-long exp_long(long x);
-unsigned long exp_fudged(long x, int fudge);
-long reg_div_float(long x, long y);
-long log_float14(unsigned long x);
-long reg_fg_to_float(long x, int fudge_factor);
-long reg_sft_float(long x, int shift);
