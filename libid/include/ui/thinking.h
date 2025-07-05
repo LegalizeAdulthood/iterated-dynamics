3786029b9c3c3bbd9c88fd0c04fe8117dc98e08b
Richard <legalize@xmission.com>
2025-07-04 19:12:20 -0600

    Add Thinking RAII helper class

--- libid/include/ui/thinking.h
+++ libid/include/ui/thinking.h
@@ -3,3 +3,29 @@
 #pragma once
 
 bool thinking(int options, const char *msg);
+
+class Thinking
+{
+public:
+    Thinking(int options, const char *msg) :
+        m_thinking(true)
+    {
+        thinking(options, msg);
+    }
+
+    ~Thinking()
+    {
+        if (m_thinking)
+        {
+            thinking(0, nullptr);
+        }
+    }
+
+    void cancel()
+    {
+        m_thinking = false;
+    }
+
+private:
+    bool m_thinking{};
+};
