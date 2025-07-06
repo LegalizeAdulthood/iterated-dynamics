49b553dc5bdb5fb98644cdca00580add96fe2187
Richard <legalize@xmission.com>
2025-07-05 21:10:38 -0600

    Extract Class Froth from frothy basin functions

--- libid/fractals/frothy_basin.cpp
+++ libid/fractals/frothy_basin.cpp
@@ -24,37 +24,48 @@ constexpr double SQRT3{1.732050807568877193};           //
 constexpr double FROTH_SLOPE{SQRT3};                    //
 constexpr double FROTH_CRITICAL_A{1.028713768218725};   // 1.0287137682187249127
 
-struct Froth
+class Froth
 {
-    int repeat_mapping;
-    int alt_color;
-    int attractors;
-    int shades;
-    double a;
-    double half_a;
-    double top_x1;
-    double top_x2;
-    double top_x3;
-    double top_x4;
-    double left_x1;
-    double left_x2;
-    double left_x3;
-    double left_x4;
-    double right_x1;
-    double right_x2;
-    double right_x3;
-    double right_x4;
+public:
+    Froth() = default;
+    ~Froth() = default;
+    bool per_image();
+    int orbit();
+    int calc();
+
+private:
+    double top_x_mapping(double x);
+    void set_froth_palette();
+
+    bool m_repeat_mapping{};
+    int m_alt_color{};
+    int m_attractors{};
+    int m_shades{};
+    double m_a{};
+    double m_half_a{};
+    double m_top_x1{};
+    double m_top_x2{};
+    double m_top_x3{};
+    double m_top_x4{};
+    double m_left_x1{};
+    double m_left_x2{};
+    double m_left_x3{};
+    double m_left_x4{};
+    double m_right_x1{};
+    double m_right_x2{};
+    double m_right_x3{};
+    double m_right_x4{};
 };
 
 static Froth s_fsp{};
 
-static double froth_top_x_mapping(double x)
+double Froth::top_x_mapping(double x)
 {
-    return x * x - x -3 * s_fsp.a * s_fsp.a / 4;
+    return x * x - x -3 * m_a * m_a / 4;
 }
 
 // color maps which attempt to replicate the images of James Alexander.
-static void set_froth_palette()
+void Froth::set_froth_palette()
 {
     if (g_color_state != ColorState::DEFAULT)   // 0 means g_dac_box matches default
     {
@@ -65,7 +76,7 @@ static void set_froth_palette()
         const char *map_name;
         if (g_colors >= 256)
         {
-            if (s_fsp.attractors == 6)
+            if (m_attractors == 6)
             {
                 map_name = "froth6.map";
             }
@@ -76,7 +87,7 @@ static void set_froth_palette()
         }
         else // colors >= 16
         {
-            if (s_fsp.attractors == 6)
+            if (m_attractors == 6)
             {
                 map_name = "froth616.map";
             }
@@ -94,66 +105,66 @@ static void set_froth_palette()
     }
 }
 
-bool froth_per_image()
+bool Froth::per_image()
 {
     constexpr double sin_theta = SQRT3 / 2; // sin(2*PI/3)
     constexpr double cos_theta = -0.5;      // cos(2*PI/3)
 
     // for the all important backwards compatibility
-    if (g_params[0] != 2)
+    if (g_params[0] != 2.0)
     {
         g_params[0] = 1;
     }
-    if (g_params[1] != 0)
+    if (g_params[1] != 0.0)
     {
         g_params[1] = 1;
     }
-    s_fsp.repeat_mapping = (int)g_params[0] == 2;
-    s_fsp.alt_color = (int)g_params[1];
-    s_fsp.a = g_params[2];
-    if (std::abs(s_fsp.a) <= FROTH_CRITICAL_A)
+    m_repeat_mapping = g_params[0] == 2.0;
+    m_alt_color = (int)g_params[1];
+    m_a = g_params[2];
+    if (std::abs(m_a) <= FROTH_CRITICAL_A)
     {
-        s_fsp.attractors = !s_fsp.repeat_mapping ? 3 : 6;
+        m_attractors = !m_repeat_mapping ? 3 : 6;
     }
     else
     {
-        s_fsp.attractors = !s_fsp.repeat_mapping ? 2 : 3;
+        m_attractors = !m_repeat_mapping ? 2 : 3;
     }
 
     // new improved values
     // 0.5 is the value that causes the mapping to reach a minimum
     constexpr double x0 = 0.5;
     // a/2 is the value that causes the y value to be invariant over the mappings
-    s_fsp.half_a = s_fsp.a/2;
-    const double y0 = s_fsp.half_a;
-    s_fsp.top_x1 = froth_top_x_mapping(x0);
-    s_fsp.top_x2 = froth_top_x_mapping(s_fsp.top_x1);
-    s_fsp.top_x3 = froth_top_x_mapping(s_fsp.top_x2);
-    s_fsp.top_x4 = froth_top_x_mapping(s_fsp.top_x3);
+    m_half_a = m_a/2;
+    const double y0 = m_half_a;
+    m_top_x1 = top_x_mapping(x0);
+    m_top_x2 = top_x_mapping(m_top_x1);
+    m_top_x3 = top_x_mapping(m_top_x2);
+    m_top_x4 = top_x_mapping(m_top_x3);
 
     // rotate 120 degrees counter-clock-wise
-    s_fsp.left_x1 = s_fsp.top_x1 * cos_theta - y0 * sin_theta;
-    s_fsp.left_x2 = s_fsp.top_x2 * cos_theta - y0 * sin_theta;
-    s_fsp.left_x3 = s_fsp.top_x3 * cos_theta - y0 * sin_theta;
-    s_fsp.left_x4 = s_fsp.top_x4 * cos_theta - y0 * sin_theta;
+    m_left_x1 = m_top_x1 * cos_theta - y0 * sin_theta;
+    m_left_x2 = m_top_x2 * cos_theta - y0 * sin_theta;
+    m_left_x3 = m_top_x3 * cos_theta - y0 * sin_theta;
+    m_left_x4 = m_top_x4 * cos_theta - y0 * sin_theta;
 
     // rotate 120 degrees clock-wise
-    s_fsp.right_x1 = s_fsp.top_x1 * cos_theta + y0 * sin_theta;
-    s_fsp.right_x2 = s_fsp.top_x2 * cos_theta + y0 * sin_theta;
-    s_fsp.right_x3 = s_fsp.top_x3 * cos_theta + y0 * sin_theta;
-    s_fsp.right_x4 = s_fsp.top_x4 * cos_theta + y0 * sin_theta;
+    m_right_x1 = m_top_x1 * cos_theta + y0 * sin_theta;
+    m_right_x2 = m_top_x2 * cos_theta + y0 * sin_theta;
+    m_right_x3 = m_top_x3 * cos_theta + y0 * sin_theta;
+    m_right_x4 = m_top_x4 * cos_theta + y0 * sin_theta;
 
     // if 2 attractors, use same shades as 3 attractors
-    s_fsp.shades = (g_colors-1) / std::max(3, s_fsp.attractors);
+    m_shades = (g_colors-1) / std::max(3, m_attractors);
 
     // rqlim needs to be at least sq(1+sqrt(1+sq(a))),
     // which is never bigger than 6.93..., so we'll call it 7.0
     g_magnitude_limit = std::max(g_magnitude_limit, 7.0);
     set_froth_palette();
     // make the best of the .map situation
-    if (s_fsp.attractors != 6 && g_colors >= 16)
+    if (m_attractors != 6 && g_colors >= 16)
     {
-        g_orbit_color = (s_fsp.shades << 1) + 1;
+        g_orbit_color = (m_shades << 1) + 1;
     }
     else
     {
@@ -163,8 +174,28 @@ bool froth_per_image()
     return true;
 }
 
-// Froth Fractal type
-int froth_type()   // per pixel 1/2/g, called with row & col set
+int Froth::orbit()
+{
+    g_new_z.x = g_temp_sqr_x - g_temp_sqr_y - g_old_z.x - m_a * g_old_z.y;
+    g_new_z.y = 2.0 * g_old_z.x * g_old_z.y - m_a * g_old_z.x + g_old_z.y;
+    if (m_repeat_mapping)
+    {
+        g_old_z = g_new_z;
+        g_new_z.x = sqr(g_old_z.x) - sqr(g_old_z.y) - g_old_z.x - m_a * g_old_z.y;
+        g_new_z.y = 2.0 * g_old_z.x * g_old_z.y - m_a * g_old_z.x + g_old_z.y;
+    }
+
+    g_temp_sqr_x = sqr(g_new_z.x);
+    g_temp_sqr_y = sqr(g_new_z.y);
+    if (g_temp_sqr_x + g_temp_sqr_y >= g_magnitude_limit)
+    {
+        return 1;
+    }
+    g_old_z = g_new_z;
+    return 0;
+}
+
+int Froth::calc()
 {
     int found_attractor = 0;
 
@@ -197,13 +228,13 @@ int froth_type()   // per pixel 1/2/g, called with row & col set
     {
         // simple formula: z = z^2 + conj(z*(-1+ai))
         // but it's the attractor that makes this so interesting
-        g_new_z.x = g_temp_sqr_x - g_temp_sqr_y - g_old_z.x - s_fsp.a * g_old_z.y;
-        g_old_z.y += (g_old_z.x + g_old_z.x) * g_old_z.y - s_fsp.a * g_old_z.x;
+        g_new_z.x = g_temp_sqr_x - g_temp_sqr_y - g_old_z.x - m_a * g_old_z.y;
+        g_old_z.y += (g_old_z.x + g_old_z.x) * g_old_z.y - m_a * g_old_z.x;
         g_old_z.x = g_new_z.x;
-        if (s_fsp.repeat_mapping)
+        if (m_repeat_mapping)
         {
-            g_new_z.x = sqr(g_old_z.x) - sqr(g_old_z.y) - g_old_z.x - s_fsp.a * g_old_z.y;
-            g_old_z.y += (g_old_z.x + g_old_z.x) * g_old_z.y - s_fsp.a * g_old_z.x;
+            g_new_z.x = sqr(g_old_z.x) - sqr(g_old_z.y) - g_old_z.x - m_a * g_old_z.y;
+            g_old_z.y += (g_old_z.x + g_old_z.x) * g_old_z.y - m_a * g_old_z.x;
             g_old_z.x = g_new_z.x;
         }
 
@@ -218,21 +249,21 @@ int froth_type()   // per pixel 1/2/g, called with row & col set
             plot_orbit(g_old_z.x, g_old_z.y, -1);
         }
 
-        if (std::abs(s_fsp.half_a - g_old_z.y) < FROTH_CLOSE && g_old_z.x >= s_fsp.top_x1 &&
-            g_old_z.x <= s_fsp.top_x2)
+        if (std::abs(m_half_a - g_old_z.y) < FROTH_CLOSE && g_old_z.x >= m_top_x1 &&
+            g_old_z.x <= m_top_x2)
         {
-            if ((!s_fsp.repeat_mapping && s_fsp.attractors == 2) ||
-                (s_fsp.repeat_mapping && s_fsp.attractors == 3))
+            if ((!m_repeat_mapping && m_attractors == 2) ||
+                (m_repeat_mapping && m_attractors == 3))
             {
                 found_attractor = 1;
             }
-            else if (g_old_z.x <= s_fsp.top_x3)
+            else if (g_old_z.x <= m_top_x3)
             {
                 found_attractor = 1;
             }
-            else if (g_old_z.x >= s_fsp.top_x4)
+            else if (g_old_z.x >= m_top_x4)
             {
-                if (!s_fsp.repeat_mapping)
+                if (!m_repeat_mapping)
                 {
                     found_attractor = 1;
                 }
@@ -242,20 +273,20 @@ int froth_type()   // per pixel 1/2/g, called with row & col set
                 }
             }
         }
-        else if (std::abs(FROTH_SLOPE * g_old_z.x - s_fsp.a - g_old_z.y) < FROTH_CLOSE &&
-            g_old_z.x <= s_fsp.right_x1 && g_old_z.x >= s_fsp.right_x2)
+        else if (std::abs(FROTH_SLOPE * g_old_z.x - m_a - g_old_z.y) < FROTH_CLOSE &&
+            g_old_z.x <= m_right_x1 && g_old_z.x >= m_right_x2)
         {
-            if (!s_fsp.repeat_mapping && s_fsp.attractors == 2)
+            if (!m_repeat_mapping && m_attractors == 2)
             {
                 found_attractor = 2;
             }
-            else if (s_fsp.repeat_mapping && s_fsp.attractors == 3)
+            else if (m_repeat_mapping && m_attractors == 3)
             {
                 found_attractor = 3;
             }
-            else if (g_old_z.x >= s_fsp.right_x3)
+            else if (g_old_z.x >= m_right_x3)
             {
-                if (!s_fsp.repeat_mapping)
+                if (!m_repeat_mapping)
                 {
                     found_attractor = 2;
                 }
@@ -264,9 +295,9 @@ int froth_type()   // per pixel 1/2/g, called with row & col set
                     found_attractor = 4;
                 }
             }
-            else if (g_old_z.x <= s_fsp.right_x4)
+            else if (g_old_z.x <= m_right_x4)
             {
-                if (!s_fsp.repeat_mapping)
+                if (!m_repeat_mapping)
                 {
                     found_attractor = 3;
                 }
@@ -276,20 +307,20 @@ int froth_type()   // per pixel 1/2/g, called with row & col set
                 }
             }
         }
-        else if (std::abs(-FROTH_SLOPE * g_old_z.x - s_fsp.a - g_old_z.y) < FROTH_CLOSE &&
-            g_old_z.x <= s_fsp.left_x1 && g_old_z.x >= s_fsp.left_x2)
+        else if (std::abs(-FROTH_SLOPE * g_old_z.x - m_a - g_old_z.y) < FROTH_CLOSE &&
+            g_old_z.x <= m_left_x1 && g_old_z.x >= m_left_x2)
         {
-            if (!s_fsp.repeat_mapping && s_fsp.attractors == 2)
+            if (!m_repeat_mapping && m_attractors == 2)
             {
                 found_attractor = 2;
             }
-            else if (s_fsp.repeat_mapping && s_fsp.attractors == 3)
+            else if (m_repeat_mapping && m_attractors == 3)
             {
                 found_attractor = 2;
             }
-            else if (g_old_z.x >= s_fsp.left_x3)
+            else if (g_old_z.x >= m_left_x3)
             {
-                if (!s_fsp.repeat_mapping)
+                if (!m_repeat_mapping)
                 {
                     found_attractor = 3;
                 }
@@ -298,9 +329,9 @@ int froth_type()   // per pixel 1/2/g, called with row & col set
                     found_attractor = 5;
                 }
             }
-            else if (g_old_z.x <= s_fsp.left_x4)
+            else if (g_old_z.x <= m_left_x4)
             {
-                if (!s_fsp.repeat_mapping)
+                if (!m_repeat_mapping)
                 {
                     found_attractor = 2;
                 }
@@ -335,19 +366,19 @@ int froth_type()   // per pixel 1/2/g, called with row & col set
     {
         if (g_colors >= 256)
         {
-            if (!s_fsp.alt_color)
+            if (!m_alt_color)
             {
-                g_color_iter = std::min<long>(g_color_iter, s_fsp.shades);
+                g_color_iter = std::min<long>(g_color_iter, m_shades);
             }
             else
             {
-                g_color_iter = s_fsp.shades * g_color_iter / g_max_iterations;
+                g_color_iter = m_shades * g_color_iter / g_max_iterations;
             }
             if (g_color_iter == 0)
             {
                 g_color_iter = 1;
             }
-            g_color_iter += s_fsp.shades * (found_attractor-1);
+            g_color_iter += m_shades * (found_attractor-1);
         }
         else if (g_colors >= 16)
         {
@@ -357,7 +388,7 @@ int froth_type()   // per pixel 1/2/g, called with row & col set
             // Since there are only a few possibilities, just handle each case.
             // This is a guess work here.
             long l_shade = (g_color_iter << 16) / g_max_iterations;
-            if (s_fsp.attractors != 6) // either 2 or 3 attractors
+            if (m_attractors != 6) // either 2 or 3 attractors
             {
                 if (l_shade < 2622)         // 0.04
                 {
@@ -412,12 +443,21 @@ int froth_type()   // per pixel 1/2/g, called with row & col set
     return g_color;
 }
 
-/*
-These last two froth functions are for the orbit-in-window feature.
-Normally, this feature requires standard_fractal, but since it is the
-attractor that makes the frothybasin type so unique, it is worth
-putting in as a stand-alone.
-*/
+// Froth Fractal type
+int froth_type()   // per pixel 1/2/g, called with row & col set
+{
+    return s_fsp.calc();
+}
+
+bool froth_per_image()
+{
+    return s_fsp.per_image();
+}
+
+// These last two froth functions are for the orbit-in-window feature.
+// Normally, this feature requires standard_fractal, but since it is the
+// attractor that makes the frothybasin type so unique, it is worth
+// putting in as a stand-alone.
 
 int froth_per_pixel()
 {
@@ -430,21 +470,5 @@ int froth_per_pixel()
 
 int froth_orbit()
 {
-    g_new_z.x = g_temp_sqr_x - g_temp_sqr_y - g_old_z.x - s_fsp.a * g_old_z.y;
-    g_new_z.y = 2.0 * g_old_z.x * g_old_z.y - s_fsp.a * g_old_z.x + g_old_z.y;
-    if (s_fsp.repeat_mapping)
-    {
-        g_old_z = g_new_z;
-        g_new_z.x = sqr(g_old_z.x) - sqr(g_old_z.y) - g_old_z.x - s_fsp.a * g_old_z.y;
-        g_new_z.y = 2.0 * g_old_z.x * g_old_z.y - s_fsp.a * g_old_z.x + g_old_z.y;
-    }
-
-    g_temp_sqr_x = sqr(g_new_z.x);
-    g_temp_sqr_y = sqr(g_new_z.y);
-    if (g_temp_sqr_x + g_temp_sqr_y >= g_magnitude_limit)
-    {
-        return 1;
-    }
-    g_old_z = g_new_z;
-    return 0;
+    return s_fsp.orbit();
 }
