f05b383c9fcadf121b2aa6000a47a255d57740d8
Richard <legalize@xmission.com>
2025-07-12 22:58:32 -0600

    Replace snprintf with fmt::format in help_title.cpp

--- libid/ui/help_title.cpp
+++ libid/ui/help_title.cpp
@@ -9,26 +9,22 @@
 
 #include <config/port_config.h>
 
-#include <cstdio>
-#include <cstring>
+#include <fmt/format.h>
+
+#include <string>
 
 void help_title()
 {
     driver_set_clear();
-    char msg[MSG_LEN];
-    std::snprintf(msg, sizeof(msg), ID_PROGRAM_NAME " Version %d.%d", ID_VERSION_MAJOR, ID_VERSION_MINOR);
+    std::string msg{fmt::format(ID_PROGRAM_NAME " Version {:d}.{:d}", ID_VERSION_MAJOR, ID_VERSION_MINOR)};
     if (ID_VERSION_PATCH)
     {
-        char buf[MSG_LEN];
-        std::snprintf(buf, sizeof(buf), ".%d", ID_VERSION_PATCH);
-        std::strcat(msg, buf);
+        msg += fmt::format(".{:d}", ID_VERSION_PATCH);
     }
     if (ID_VERSION_TWEAK)
     {
-        char buf[MSG_LEN];
-        std::snprintf(buf, sizeof(buf), ".%d", ID_VERSION_TWEAK);
-        std::strcat(msg, buf);
+        msg += fmt::format(".{:d}", ID_VERSION_TWEAK);
     }
-    std::strcat(msg, " (" ID_GIT_HASH ")");
+    msg += " (" ID_GIT_HASH ")";
     put_string_center(0, 0, 80, C_TITLE, msg);
 }
