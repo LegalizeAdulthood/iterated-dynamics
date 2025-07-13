690fb3499b5452e1ad6d5cbe71ec29964de94f00
Richard <legalize@xmission.com>
2025-07-12 22:58:18 -0600

    Add std::string overload in put_string_center.h

--- libid/include/ui/put_string_center.h
+++ libid/include/ui/put_string_center.h
@@ -2,4 +2,11 @@
 //
 #pragma once
 
+#include <string>
+
 int put_string_center(int row, int col, int width, int attr, const char *msg);
+
+inline int put_string_center(int row, int col, int width, int attr, const std::string &msg)
+{
+    return put_string_center(row, col, width, attr, msg.c_str());
+}
