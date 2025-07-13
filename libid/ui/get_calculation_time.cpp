63948f694276fadf53d1ff7da74e13386f6c4fc9
Richard <legalize@xmission.com>
2025-07-12 22:42:02 -0600

    Replacesnprintf with fmt::format in get_calculation_time.cpp

--- libid/ui/get_calculation_time.cpp
+++ libid/ui/get_calculation_time.cpp
@@ -2,8 +2,8 @@
 //
 #include "ui/get_calculation_time.h"
 
-#include <array>
-#include <cstdio>
+#include <fmt/format.h>
+
 #include <string>
 
 std::string get_calculation_time(long calc_time)
@@ -13,8 +13,9 @@ std::string get_calculation_time(long calc_time)
         return "A long time! (> 24.855 days)";
     }
 
-    char msg[80];
-    std::snprintf(msg, std::size(msg), "%3ld:%02ld:%02ld.%02ld", calc_time/360000L,
-        (calc_time%360000L)/6000, (calc_time%6000)/100, calc_time%100);
-    return msg;
+    return fmt::format("{:3d}:{:02d}:{:02d}.{:02d}", //
+        calc_time / 360000L,                         //
+        (calc_time % 360000L) / 6000,                //
+        (calc_time % 6000) / 100,                    //
+        calc_time % 100);
 }
