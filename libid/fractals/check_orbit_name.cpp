5ae5bd560d6901a9aa2786d2b8723aefc61decdb
Richard <legalize@xmission.com>
2025-07-21 21:07:23 -0400

    Simplify check_orbit_name to scan fractal type list only once
    No point in building a separate array of types that are julibrot
    compatible only to scan it again looking for a matching name.

--- libid/fractals/check_orbit_name.cpp
+++ libid/fractals/check_orbit_name.cpp
@@ -7,46 +7,16 @@
 
 #include <cstring>
 
-enum
-{
-    MAX_FRACTALS = 25
-};
-
-static int build_fractal_list(FractalType fractals[], const char *name_ptr[])
-{
-    int num_fractals = 0;
-    for (int i = 0; i < g_num_fractal_types; i++)
-    {
-        const FractalSpecific &fs{g_fractal_specific[i]};
-        if (bit_set(fs.flags, FractalFlags::OK_JB))
-        {
-            fractals[num_fractals] = fs.type;
-            name_ptr[num_fractals] = fs.name;
-            num_fractals++;
-            if (num_fractals >= MAX_FRACTALS)
-            {
-                break;
-            }
-        }
-    }
-    return num_fractals;
-}
-
 bool check_orbit_name(const char *orbit_name)
 {
-    const char *names[MAX_FRACTALS];
-    FractalType types[MAX_FRACTALS];
-
-    int num_types = build_fractal_list(types, names);
-    bool bad = true;
-    for (int i = 0; i < num_types; i++)
+    for (int i = 0; i < g_num_fractal_types; i++)
     {
-        if (std::strcmp(orbit_name, names[i]) == 0)
+        if (const FractalSpecific & fs{g_fractal_specific[i]};
+            bit_set(fs.flags, FractalFlags::OK_JB) && std::strcmp(orbit_name, fs.name) == 0)
         {
-            g_new_orbit_type = types[i];
-            bad = false;
-            break;
+            g_new_orbit_type = fs.type;
+            return false; // found a match
         }
     }
-    return bad;
+    return true;
 }
