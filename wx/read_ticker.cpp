a826cb02f71220c444d537b12c2b7065e0d8a268
Richard <legalize@xmission.com>
2025-08-18 01:23:28 -0600

    Implement read_ticker using wxDateTime

--- wx/read_ticker.cpp
+++ wx/read_ticker.cpp
@@ -1,11 +1,11 @@
 #include "ui/read_ticker.h"
 
-#include <wx/app.h>
+#include <wx/datetime.h>
 
 /*
 ; long read_ticker() returns current bios ticker value
 */
 long read_ticker()
 {
-    return (long) GetTickCount();
+    return wxDateTime::Now().GetValue().ToLong();
 }
