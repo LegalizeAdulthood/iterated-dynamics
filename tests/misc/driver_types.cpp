dd90135090d9e75aadcbc9a47e42f1e90ced7f65
Richard <legalize@xmission.com>
2025-08-23 23:39:52 -0600

    Stub out driver type functions
    Each driver is supplied as a pointer to a distinct mock driver

---
+++ tests/misc/driver_types.cpp
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-3.0-only
+//
+#include <config/driver_types.h>
+
+#include "MockDriver.h"
+
+#if ID_HAVE_X11_DRIVER
+static MockDriver s_x11_driver;
+
+Driver *get_x11_driver()
+{
+    return &s_x11_driver;
+}
+#endif
+
+#if ID_HAVE_GDI_DRIVER
+static MockDriver s_gdi_driver;
+
+Driver *get_gdi_driver()
+{
+    return &s_gdi_driver;
+}
+#endif
+
+#if ID_HAVE_WIN32_DISK_DRIVER
+static MockDriver s_disk_driver;
+
+Driver *get_disk_driver()
+{
+    return &s_disk_driver;
+}
+#endif
+
+#if ID_HAVE_WX_DRIVER
+static MockDriver s_wx_driver;
+
+Driver *get_wx_driver()
+{
+    return &s_wx_driver;
+}
+#endif
