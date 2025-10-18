4d371f432f90c230e97ae38c4fb1c9a6b2c13e5e
Richard <legalize@xmission.com>
2025-10-18 17:14:13 -0600

    Add operator ==, != for Point<T>

--- libid/include/math/Point.h
+++ libid/include/math/Point.h
@@ -11,6 +11,19 @@ struct Point2
     T x;
     T y;
 };
+
+template <typename T>
+bool operator==(const Point2<T> &lhs, const Point2<T> &rhs)
+{
+    return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+template <typename T>
+bool operator!=(const Point2<T> &lhs, const Point2<T> &rhs)
+{
+    return !(lhs == rhs);
+}
+
 using Point2i = Point2<int>;
 
 } // namespace id::math
