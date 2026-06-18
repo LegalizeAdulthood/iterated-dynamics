3851b2ce05729fabf9f32a803be869844a672b8c
Richard <legalize@xmission.com>
2026-06-17 21:09:12 -0600

    Emit xid for non-Windows GUI builds
    Add an optional RUNTIME_NAME argument to gui_executable() so callers can
    set the output name without baking target-specific policy into the helper.
    Use it to emit xid for X11 builds and for wx builds on non-Windows
    platforms, avoiding the POSIX id utility collision on macOS and Linux.
    
    Refs #42

--- cmake/gui_executable.cmake
+++ cmake/gui_executable.cmake
@@ -3,8 +3,9 @@
 # Copyright 2025-2026 Richard Thomson
 #
 function(gui_executable target)
+    cmake_parse_arguments(ARG "" "RUNTIME_NAME" "" ${ARGN})
     set_target_properties("${target}" PROPERTIES WIN32_EXECUTABLE $<PLATFORM_ID:Windows>)
-    if(CMAKE_SYSTEM_NAME STREQUAL "Linux" AND target STREQUAL "id")
-        set_target_properties("${target}" PROPERTIES RUNTIME_OUTPUT_NAME xid)
+    if(DEFINED ARG_RUNTIME_NAME)
+        set_target_properties("${target}" PROPERTIES RUNTIME_OUTPUT_NAME "${ARG_RUNTIME_NAME}")
     endif()
 endfunction()
