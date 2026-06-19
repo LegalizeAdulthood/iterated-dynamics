ad03ecd48352f3a7f8719c84a945b3c734416590
github-pages <pages-bot@example.com>
2026-06-19 02:28:22 +0000

    Update developer reports for 73b019a9cf7b6582111834cec64fc4c33ea5e185

--- dev/doxygen/search/defines_8.js
+++ dev/doxygen/search/defines_8.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['nominmax_0',['NOMINMAX',['../df/dad/win__defines_8h.html#a9f918755b601cf4bffca775992e6fb90',1,'win_defines.h']]]
+  ['nominmax_0',['nominmax',['../df/d67/SpecialDirectoriesWin_8cpp.html#a9f918755b601cf4bffca775992e6fb90',1,'NOMINMAX:&#160;SpecialDirectoriesWin.cpp'],['../df/dad/win__defines_8h.html#a9f918755b601cf4bffca775992e6fb90',1,'NOMINMAX:&#160;win_defines.h']]]
 ];
--- dev/doxygen/search/defines_e.js
+++ dev/doxygen/search/defines_e.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['width_0',['WIDTH',['../d6/d21/stereo_8cpp.html#a241aeeb764887ae5e3de58b98f04b16d',1,'stereo.cpp']]],
-  ['win32_5flean_5fand_5fmean_1',['WIN32_LEAN_AND_MEAN',['../df/dad/win__defines_8h.html#ac7bef5d85e3dcd73eef56ad39ffc84a9',1,'win_defines.h']]]
+  ['win32_5flean_5fand_5fmean_1',['win32_lean_and_mean',['../df/d67/SpecialDirectoriesWin_8cpp.html#ac7bef5d85e3dcd73eef56ad39ffc84a9',1,'WIN32_LEAN_AND_MEAN:&#160;SpecialDirectoriesWin.cpp'],['../df/dad/win__defines_8h.html#ac7bef5d85e3dcd73eef56ad39ffc84a9',1,'WIN32_LEAN_AND_MEAN:&#160;win_defines.h']]]
 ];
